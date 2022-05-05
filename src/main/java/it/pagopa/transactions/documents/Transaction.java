9b60cf39ee287251535bc709165566be389f25b3
Giovanni Berti <giovanni.berti@pagopa.it>
2022-05-05 10:28:21 +0200

    refactor(transaction): use paymentToken as id inside view

--- src/main/java/it/pagopa/transactions/documents/Transaction.java
+++ src/main/java/it/pagopa/transactions/documents/Transaction.java
@@ -1,31 +1,24 @@
 package it.pagopa.transactions.documents;
 
-import java.time.ZonedDateTime;
-import java.util.UUID;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import it.pagopa.transactions.utils.TransactionStatus;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import static java.time.ZonedDateTime.now;
 
 @Data
 @Document(collection = "view")
 public class Transaction {
-
     @Id
-    private String id;
-    private String rptId;
     private String paymentToken;
+    private String rptId;
     private String description;
     private int amount;
     private TransactionStatus status;
     private String creationDate;
 
     public Transaction(String paymentToken, String rptId, String description, int amount, TransactionStatus status) {
-        this.id = UUID.randomUUID().toString();
         this.rptId = rptId;
         this.description = description;
         this.paymentToken = paymentToken;
