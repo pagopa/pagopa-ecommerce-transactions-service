c19b466474206fae1130a998a30acd6370b65798
Simone Infante <simone.infante@pagopa.it>
2022-05-18 16:54:50 +0200

    fix: test

--- src/test/java/it/pagopa/transactions/TransactionUtilTest.java
+++ src/test/java/it/pagopa/transactions/TransactionUtilTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TransactionUtilTest {
+class TransactionUtilTest {
 
     @Test
-    public void transactionUtilitiesTest(){
+    void transactionUtilitiesTest(){
         assertEquals("TRANSACTION_INITIALIZED_EVENT", TransactionEventCode.TRANSACTION_INITIALIZED_EVENT.toString());
         assertEquals("TRANSACTION_INITIALIZED", TransactionStatus.TRANSACTION_INITIALIZED.getCode());
     }
