c1a3e661f4df38f15799ebea107104a2187c8a0b
MarcoAGreco <marcoandreagreco@gmail.com>
2022-05-18 14:45:39 +0200

    [CHK-383] fix: add annotation for jacoco

--- src/main/java/it/pagopa/transactions/projections/TransactionsProjection.java
+++ src/main/java/it/pagopa/transactions/projections/TransactionsProjection.java
@@ -3,11 +3,13 @@ package it.pagopa.transactions.projections;
 import it.pagopa.transactions.model.RptId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Generated
 public class TransactionsProjection<T> {
     
     private RptId rptId;
