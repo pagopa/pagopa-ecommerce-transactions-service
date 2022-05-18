5d658d2e60507be4200d15e42071f2c605482499
MarcoAGreco <marcoandreagreco@gmail.com>
2022-05-18 14:43:21 +0200

    [CHK-383] fix: add annotation for jacoco

--- src/main/java/it/pagopa/transactions/model/SoapEnvelope.java
+++ src/main/java/it/pagopa/transactions/model/SoapEnvelope.java
@@ -4,12 +4,14 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 @XmlRootElement
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Generated
 public class SoapEnvelope {
 
     private String header;
