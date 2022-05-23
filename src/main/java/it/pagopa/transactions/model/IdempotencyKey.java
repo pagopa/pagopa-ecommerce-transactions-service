b3d2acb8883b36edda60f4e740eff7b0ef9ed028
Giovanni Berti <giovanni.berti@pagopa.it>
2022-05-23 16:44:44 +0200

    refactor(idempotencykey): annotate idempotency key as value object

--- src/main/java/it/pagopa/transactions/model/IdempotencyKey.java
+++ src/main/java/it/pagopa/transactions/model/IdempotencyKey.java
@@ -1,12 +1,16 @@
 package it.pagopa.transactions.model;
 
-import java.util.Objects;
+import it.pagopa.transactions.annotations.ValueObject;
+import lombok.Value;
+
 import java.util.regex.Pattern;
 
-public final class IdempotencyKey {
+@ValueObject
+@Value
+public class IdempotencyKey {
     private static final Pattern pspFiscalCodeRegex = Pattern.compile("\\d{11}");
     private static final Pattern keyIdentifierRegex = Pattern.compile("[a-zA-Z\\d]{10}");
-    private final String key;
+    String key;
 
     public IdempotencyKey(String pspFiscalCode, String keyIdentifier) {
         if (!pspFiscalCodeRegex.matcher(pspFiscalCode).matches()) {
@@ -19,28 +23,4 @@ public final class IdempotencyKey {
 
         this.key = pspFiscalCode + "_" + keyIdentifier;
     }
-
-    public String getKey() {
-        return key;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (IdempotencyKey) obj;
-        return Objects.equals(this.key, that.key);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(key);
-    }
-
-    @Override
-    public String toString() {
-        return "IdempotencyKey[" +
-                "value=" + key + ']';
-    }
-
 }
