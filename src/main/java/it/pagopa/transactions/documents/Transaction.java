1f56922dcc2792a37f9f942b2ec9ad35c23a6750
Giovanni Berti <giovanni.berti@pagopa.it>
2022-05-24 09:59:57 +0200

    refactor(transaction): add transaction id to transaction document

--- src/main/java/it/pagopa/transactions/documents/Transaction.java
+++ src/main/java/it/pagopa/transactions/documents/Transaction.java
@@ -2,16 +2,20 @@ package it.pagopa.transactions.documents;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import it.pagopa.generated.transactions.server.model.TransactionStatusDto;
 
 import java.time.ZonedDateTime;
+import java.util.UUID;
 
 @Data
 @Document(collection = "view")
 public class Transaction {
     @Id
+    private String transactionId;
+
     private String paymentToken;
     private String rptId;
     private String description;
@@ -20,10 +24,12 @@ public class Transaction {
     private String creationDate;
 
     public Transaction(String paymentToken, String rptId, String description, int amount, TransactionStatusDto status) {
-        this(paymentToken, rptId, description, amount, status, ZonedDateTime.now());
+        this(UUID.randomUUID().toString(), paymentToken, rptId, description, amount, status, ZonedDateTime.now());
     }
 
-    public Transaction(String paymentToken, String rptId, String description, int amount, TransactionStatusDto status, ZonedDateTime creationDate) {
+    @PersistenceConstructor
+    public Transaction(String transactionId, String paymentToken, String rptId, String description, int amount, TransactionStatusDto status, ZonedDateTime creationDate) {
+        this.transactionId = transactionId;
         this.rptId = rptId;
         this.description = description;
         this.paymentToken = paymentToken;
@@ -34,6 +40,7 @@ public class Transaction {
 
     public static Transaction from(it.pagopa.transactions.model.Transaction transaction) {
         return new Transaction(
+                transaction.getTransactionId().value(),
                 transaction.getPaymentToken().value(),
                 transaction.getRptId().value(),
                 transaction.getDescription().value(),
--- src/test/java/it/pagopa/transactions/documents/TransactionDocumentTest.java
+++ src/test/java/it/pagopa/transactions/documents/TransactionDocumentTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.ZonedDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -15,23 +17,26 @@ class TransactionDocumentTest {
 
     @Test
     void shouldGetAndSetTransaction(){
+        String TEST_ID = "id";
         String TEST_TOKEN = "token1";
         String TEST_RPTID = "77777777777302016723749670035";
         String TEST_DESC = "";
+        ZonedDateTime TEST_TIME = ZonedDateTime.now();
         int TEST_AMOUNT = 1;
         TransactionStatusDto TEST_STATUS = TransactionStatusDto.INITIALIZED;
 
         /**
          * Test
          */
-        Transaction transaction = new Transaction(TEST_TOKEN, TEST_RPTID, TEST_DESC, TEST_AMOUNT, TEST_STATUS);
+        Transaction transaction = new Transaction(TEST_ID, TEST_TOKEN, TEST_RPTID, TEST_DESC, TEST_AMOUNT, TEST_STATUS, TEST_TIME);
 
-        Transaction sameTransaction = new Transaction(TEST_TOKEN, TEST_RPTID, TEST_DESC, TEST_AMOUNT, TEST_STATUS);
+        Transaction sameTransaction = new Transaction(TEST_ID, TEST_TOKEN, TEST_RPTID, TEST_DESC, TEST_AMOUNT, TEST_STATUS, TEST_TIME);
         sameTransaction.setCreationDate(transaction.getCreationDate());
 
         // Different transaction (creation date)
         Transaction differentTransaction = new Transaction(
-                "", "", "", 1, null);
+                "", "", "", "", 1, null, ZonedDateTime.now());
+        differentTransaction.setTransactionId(TEST_ID);
         differentTransaction.setPaymentToken(TEST_TOKEN);
         differentTransaction.setRptId(TEST_RPTID);
         differentTransaction.setDescription(TEST_DESC);
@@ -41,14 +46,15 @@ class TransactionDocumentTest {
         /**
          * Assertions
          */
+        assertEquals(TEST_ID, transaction.getTransactionId());
         assertEquals(TEST_TOKEN, transaction.getPaymentToken());
         assertEquals(TEST_RPTID, transaction.getRptId());
         assertEquals(TEST_DESC, transaction.getDescription());
         assertEquals(TEST_AMOUNT, transaction.getAmount());
         assertEquals(TEST_STATUS, transaction.getStatus());
 
-        assertFalse(transaction.equals(differentTransaction));
+        assertNotEquals(transaction, differentTransaction);
         assertEquals(transaction.hashCode(), sameTransaction.hashCode());
-        assertFalse(transaction.toString().equals(differentTransaction.toString()));
+        assertNotEquals(transaction.toString(), differentTransaction.toString());
     }   
 }
