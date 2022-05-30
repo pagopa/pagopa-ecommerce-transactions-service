7899866670833e528d9b417709f79376428f8b25
Giovanni Berti <giovanni.berti@pagopa.it>
2022-05-30 13:01:50 +0200

    refactor(handlers): refactor `TransactionsProjectionHandler` with new transaction aggregate

--- src/main/java/it/pagopa/transactions/projections/handlers/TransactionsProjectionHandler.java
+++ src/main/java/it/pagopa/transactions/projections/handlers/TransactionsProjectionHandler.java
@@ -1,15 +1,17 @@
 package it.pagopa.transactions.projections.handlers;
 
 import it.pagopa.generated.transactions.server.model.TransactionStatusDto;
+import it.pagopa.transactions.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import it.pagopa.transactions.documents.Transaction;
 import it.pagopa.transactions.repositories.TransactionsViewRepository;
 
 import it.pagopa.generated.transactions.server.model.NewTransactionResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 @Component
 @Slf4j
@@ -22,13 +24,26 @@ public class TransactionsProjectionHandler
 	@Override
 	public Mono<Transaction> handle(NewTransactionResponseDto data) {
 
-		Transaction transaction = new Transaction(data.getPaymentToken(),
-				data.getRptId(), data.getReason(),
-				data.getAmount(), TransactionStatusDto.INITIALIZED);
+		PaymentToken paymentToken = new PaymentToken(data.getPaymentToken());
+		RptId rptId = new RptId(data.getRptId());
+		TransactionDescription description = new TransactionDescription(data.getReason());
+		TransactionAmount amount = new TransactionAmount(data.getAmount());
+
+		Transaction transaction = new Transaction(
+				new TransactionId(UUID.randomUUID().toString()),
+				paymentToken,
+				rptId,
+				description,
+				amount,
+				TransactionStatusDto.INITIALIZED
+		);
+
+		it.pagopa.transactions.documents.Transaction transactionDocument = it.pagopa.transactions.documents.Transaction.from(transaction);
 
 		return viewEventStoreRepository
-				.save(transaction)
-				.doOnNext(event -> log.info("Transactions update view for rptId: {}", event.getRptId()));
+				.save(transactionDocument)
+				.doOnNext(event -> log.info("Transactions update view for rptId: {}", event.getRptId()))
+				.thenReturn(transaction);
 	}
 
 }
