7528e3e29bda1f27a221c115467766fe22700b0c
Simone Infante <simone.infante@pagopa.it>
2022-06-27 22:26:37 +0200

    fix: code smell

--- src/main/java/it/pagopa/transactions/configurations/NodoConfig.java
+++ src/main/java/it/pagopa/transactions/configurations/NodoConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
@@ -14,7 +13,7 @@ public class NodoConfig {
     @Bean
     public NodoConnectionString nodoConnectionString(
             @Value("${nodo.connection.string}") String nodoConnectionParamsAsString)
-            throws JsonMappingException, JsonProcessingException {
+            throws JsonProcessingException {
 
         return new ObjectMapper().readValue(nodoConnectionParamsAsString, NodoConnectionString.class);
     }
