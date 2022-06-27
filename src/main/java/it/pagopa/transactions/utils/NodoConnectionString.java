e988ae2b057bbbcaec1e1e6c413a0812e81eb82c
Simone Infante <simone.infante@pagopa.it>
2022-06-27 22:21:45 +0200

    feat: update tests

--- src/main/java/it/pagopa/transactions/utils/NodoConnectionString.java
+++ src/main/java/it/pagopa/transactions/utils/NodoConnectionString.java
@@ -2,9 +2,11 @@ package it.pagopa.transactions.utils;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class NodoConnectionString {
 
     String idPSP;
