d77dfbe1144276c1ec4cc48d18dad104f42d2e38
Simone Infante <simone.infante@pagopa.it>
2022-06-30 10:46:26 +0200

    fix: remove public in test

--- src/test/java/it/pagopa/transactions/domain/TransactionTest.java
+++ src/test/java/it/pagopa/transactions/domain/TransactionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TransactionTest {
+class TransactionTest {
     @Test
     void shouldConstructTransaction() {
         TransactionId transactionId = new TransactionId("");
