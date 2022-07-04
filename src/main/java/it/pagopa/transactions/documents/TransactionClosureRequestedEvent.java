5b38f23970740653e8b701407238a92cb5ffb728
Giovanni Berti <giovanni.berti@pagopa.it>
2022-07-04 16:28:44 +0200

    refactor: rename event OUTCOME_UPDATED -> CLOSURE_REQUESTED

--- src/main/java/it/pagopa/transactions/documents/TransactionClosureRequestedEvent.java
+++ src/main/java/it/pagopa/transactions/documents/TransactionClosureRequestedEvent.java
@@ -6,6 +6,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "eventstore")
 public final class TransactionClosureRequestedEvent extends TransactionEvent<TransactionClosureRequestData> {
     public TransactionClosureRequestedEvent(String rptId, String paymentToken, TransactionClosureRequestData data) {
-        super(rptId, paymentToken, TransactionEventCode.TRANSACTION_OUTCOME_UPDATED_EVENT, data);
+        super(rptId, paymentToken, TransactionEventCode.TRANSACTION_CLOSURE_REQUESTED_EVENT, data);
     }
 }
