2861aee7520df296d3884f819d8d69c71863465f
Giovanni Berti <giovanni.berti@pagopa.it>
2022-07-05 11:11:30 +0200

    test: add test for `AuthorizationUpdateProjectionHandler`

---
+++ src/test/java/it/pagopa/transactions/projections/handlers/AuthorizationUpdateProjectionHandlerTest.java
@@ -0,0 +1,82 @@
+package it.pagopa.transactions.projections.handlers;
+
+import it.pagopa.generated.transactions.server.model.AuthorizationResultDto;
+import it.pagopa.generated.transactions.server.model.TransactionStatusDto;
+import it.pagopa.generated.transactions.server.model.UpdateAuthorizationRequestDto;
+import it.pagopa.transactions.commands.data.UpdateAuthorizationStatusData;
+import it.pagopa.transactions.domain.*;
+import it.pagopa.transactions.repositories.TransactionsViewRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.OffsetDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+
+@ExtendWith(MockitoExtension.class)
+class AuthorizationUpdateProjectionHandlerTest {
+
+    @InjectMocks
+    private AuthorizationUpdateProjectionHandler authorizationUpdateProjectionHandler;
+
+    @Mock
+    private TransactionsViewRepository viewRepository;
+
+    @Test
+    void shouldHandleTransaction() {
+        UpdateAuthorizationRequestDto updateAuthorizationRequest = new UpdateAuthorizationRequestDto()
+                .authorizationResult(AuthorizationResultDto.OK)
+                .authorizationCode("OK")
+                .timestampOperation(OffsetDateTime.now());
+
+        Transaction transaction = new Transaction(
+                new PaymentToken("paymentToken"),
+                new RptId("rptId"),
+                new TransactionDescription("description"),
+                new TransactionAmount(100),
+                TransactionStatusDto.AUTHORIZATION_REQUESTED
+        );
+
+        UpdateAuthorizationStatusData updateAuthorizationStatusData = new UpdateAuthorizationStatusData(
+                transaction,
+                updateAuthorizationRequest
+        );
+
+        it.pagopa.transactions.documents.Transaction expected = new it.pagopa.transactions.documents.Transaction(
+                transaction.getPaymentToken().value(),
+                transaction.getRptId().value(),
+                transaction.getDescription().value(),
+                transaction.getAmount().value(),
+                TransactionStatusDto.AUTHORIZED,
+                transaction.getCreationDate()
+        );
+
+        /*
+         * Preconditions
+         */
+        Mockito.when(viewRepository.findByPaymentToken(transaction.getPaymentToken().value()))
+                .thenReturn(Mono.just(it.pagopa.transactions.documents.Transaction.from(transaction)));
+
+        Mockito.when(viewRepository.save(expected)).thenReturn(Mono.just(expected));
+
+        /*
+         * Test
+         */
+        StepVerifier.create(authorizationUpdateProjectionHandler.handle(updateAuthorizationStatusData))
+                .expectNext(expected)
+                .verifyComplete();
+
+        /*
+         * Assertions
+         */
+        Mockito.verify(viewRepository, Mockito.times(1)).save(argThat(savedTransaction -> savedTransaction.getStatus().equals(TransactionStatusDto.AUTHORIZED)));
+    }
+}
