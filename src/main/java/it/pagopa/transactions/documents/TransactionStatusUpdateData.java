72ae40bd51d6d60c9862943da3a1a462a4d23272
Simone Infante <simone.infante@pagopa.it>
2022-07-09 15:51:20 +0200

    feat: add event handler for update

---
+++ src/main/java/it/pagopa/transactions/documents/TransactionStatusUpdateData.java
@@ -0,0 +1,15 @@
+package it.pagopa.transactions.documents;
+
+import it.pagopa.generated.transactions.server.model.AuthorizationResultDto;
+import it.pagopa.generated.transactions.server.model.TransactionStatusDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@Data
+@Document
+public class TransactionStatusUpdateData {
+    private AuthorizationResultDto authorizationResult;
+    private TransactionStatusDto newTransactionStatus;
+}
