ed28640a05d19ffa843994993f7f11956632fc42
Simone Infante <simone.infante@pagopa.it>
2022-07-12 09:31:12 +0200

    feat: refactoring configuration

--- src/main/java/it/pagopa/transactions/configurations/AzureStorageConfig.java
+++ src/main/java/it/pagopa/transactions/configurations/AzureStorageConfig.java
@@ -1,6 +1,6 @@
 package it.pagopa.transactions.configurations;
 
-import com.azure.storage.queue.QueueClient;
+import com.azure.storage.queue.QueueAsyncClient;
 import com.azure.storage.queue.QueueClientBuilder;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -8,16 +8,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class StorageConfig {
+public class AzureStorageConfig {
 
     @Bean
-    public QueueClient initEventsQueueClient(
-            @Value("${storage.connectionstring}") String storageConnectionString,
-            @Value("${storage.queues.initevents.name}") String queueEventInitName) {
-        QueueClient queueClient = new QueueClientBuilder()
+    public QueueAsyncClient initEventsQueueClient(
+            @Value("${azurestorage.connectionstring}") String storageConnectionString,
+            @Value("${azurestorage.queues.transactioninitevents.name}") String queueEventInitName) {
+        QueueAsyncClient queueClient = new QueueClientBuilder()
                 .connectionString(storageConnectionString)
                 .queueName(queueEventInitName)
-                .buildClient();
+                .buildAsyncClient();
         queueClient.createIfNotExists();
         return queueClient;
     }
