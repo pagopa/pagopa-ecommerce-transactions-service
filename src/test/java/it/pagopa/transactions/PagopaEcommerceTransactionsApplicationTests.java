6b41026c5961ddbaea78c86f4be8f2a1ec90bbee
Simone Infante <simone.infante@pagopa.it>
2022-07-14 23:50:31 +0200

    add azurite to test CR

--- src/test/java/it/pagopa/transactions/PagopaEcommerceTransactionsApplicationTests.java
+++ src/test/java/it/pagopa/transactions/PagopaEcommerceTransactionsApplicationTests.java
@@ -1,21 +1,21 @@
-// package it.pagopa.transactions;
+package it.pagopa.transactions;
 
-// import it.pagopa.transactions.repositories.TransactionTokensRepository;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.test.context.TestPropertySource;
+import it.pagopa.transactions.repositories.TransactionTokensRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-// import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-// @SpringBootTest
-// @TestPropertySource(locations = "classpath:application-tests.properties")
-// class PagopaEcommerceTransactionsApplicationTests {
-//     @Autowired
-//     private TransactionTokensRepository repository;
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-tests.properties")
+class PagopaEcommerceTransactionsApplicationTests {
+    @Autowired
+    private TransactionTokensRepository repository;
 
-//     @Test
-//     void contextLoads() {
-//         assertNotNull(repository);
-//     }
-// }
+    @Test
+    void contextLoads() {
+        assertNotNull(repository);
+    }
+}
