cec0331f21582d6cda506b6de88d40db2c134979
Simone Infante <simone.infante@pagopa.it>
2022-08-24 15:41:01 +0200

    feat(service): new service for api paymentInfo

---
+++ src/main/java/it/pagopa/transactions/repositories/PaymentRequestInfo.java
@@ -0,0 +1,21 @@
+package it.pagopa.transactions.repositories;
+
+import it.pagopa.transactions.domain.IdempotencyKey;
+import it.pagopa.transactions.domain.RptId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+import javax.validation.constraints.Pattern;
+import java.math.BigDecimal;
+
+@RedisHash(value = "keys", timeToLive = 10 * 60)
+public record PaymentRequestInfo(@Id RptId id, String paTaxCode, String paName,
+                                 String description, BigDecimal amount,
+                                 @Pattern(regexp = "([a-zA-Z\\d]{1,35})|(RF\\d{2}[a-zA-Z\\d]{1,21})")
+                                 String dueDate,
+                                 Boolean isNM3) {
+    @PersistenceConstructor
+    public PaymentRequestInfo {
+    }
+}
