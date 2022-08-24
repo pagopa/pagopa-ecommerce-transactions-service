6eb166f2d0b141137acc686d1cdd8da80c711612
Simone Infante <simone.infante@pagopa.it>
2022-08-24 16:44:49 +0200

    feat(502): generic 502 handler

--- src/main/java/it/pagopa/transactions/controllers/PaymentRequestsController.java
+++ src/main/java/it/pagopa/transactions/controllers/PaymentRequestsController.java
@@ -1,11 +1,19 @@
 package it.pagopa.transactions.controllers;
 
+import io.lettuce.core.RedisConnectionException;
 import it.pagopa.generated.payment.requests.api.PaymentRequestsApi;
 import it.pagopa.generated.payment.requests.model.PaymentRequestsGetResponseDto;
+import it.pagopa.generated.transactions.server.model.ProblemJsonDto;
+import it.pagopa.transactions.exceptions.BadGatewayException;
+import it.pagopa.transactions.exceptions.UnsatisfiablePspRequestException;
 import it.pagopa.transactions.services.PaymentRequestsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.RedisSystemException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -20,5 +28,15 @@ public class PaymentRequestsController implements PaymentRequestsApi {
     return Mono.just(rptId)
         .flatMap(paymentRequestsService::getPaymentRequestInfo)
         .map(ResponseEntity::ok);
+  }
+
+  @ExceptionHandler({
+    RedisSystemException.class,
+    RedisConnectionException.class,
+    WebClientRequestException.class
+  })
+  private ResponseEntity<ProblemJsonDto> genericBadGatewayHandler(RuntimeException exception) {
+    return new ResponseEntity<>(
+        new ProblemJsonDto().status(502).title("Bad gateway"), HttpStatus.BAD_GATEWAY);
   }
 }
