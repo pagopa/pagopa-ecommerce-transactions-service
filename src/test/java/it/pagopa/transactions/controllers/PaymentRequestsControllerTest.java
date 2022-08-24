d8bd959b1f4930c60f326877eb653e96274c601b
Simone Infante <simone.infante@pagopa.it>
2022-08-24 22:42:47 +0200

    fix(tests): fix tests

--- src/test/java/it/pagopa/transactions/controllers/PaymentRequestsControllerTest.java
+++ src/test/java/it/pagopa/transactions/controllers/PaymentRequestsControllerTest.java
@@ -2,9 +2,11 @@ package it.pagopa.transactions.controllers;
 
 import it.pagopa.generated.payment.requests.model.PaymentRequestsGetResponseDto;
 import it.pagopa.generated.transactions.server.model.*;
+import it.pagopa.transactions.services.PaymentRequestsService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
@@ -13,20 +15,28 @@ import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PaymentRequestsControllerTest {
 
-    @InjectMocks
-    private PaymentRequestsController paymentRequestsController;
+  @InjectMocks private PaymentRequestsController paymentRequestsController;
 
-    @Test
-    void shouldGetOk() {
-        String RPTID = "77777777777302016723749670035";
+  @Mock private PaymentRequestsService paymentRequestsService;
 
-        ResponseEntity<PaymentRequestsGetResponseDto> responseEntity = paymentRequestsController
-                .getPaymentRequestInfo(RPTID, null).block();
+  @Test
+  void shouldGetPaymentInfoGivenValidRptid() {
+    String RPTID = "77777777777302016723749670035";
 
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    }
+    PaymentRequestsGetResponseDto response = new PaymentRequestsGetResponseDto();
+    response.setRptId(RPTID);
+    response.amount(1000);
+    response.setDescription("Payment test");
+    when(paymentRequestsService.getPaymentRequestInfo(RPTID)).thenReturn(Mono.just(response));
+
+    ResponseEntity<PaymentRequestsGetResponseDto> responseEntity =
+        paymentRequestsController.getPaymentRequestInfo(RPTID, null).block();
+
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+  }
 }
