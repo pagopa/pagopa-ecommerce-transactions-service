dc756df6f0075736cb895ce367a67548d09359fe
Simone Infante <simone.infante@pagopa.it>
2022-08-24 15:37:27 +0200

    feat(jaxb): add new packaget for jaxb context

--- src/main/java/it/pagopa/transactions/utils/soap/JaxbContextContainer.java
+++ src/main/java/it/pagopa/transactions/utils/soap/JaxbContextContainer.java
@@ -7,20 +7,20 @@ import javax.xml.bind.Unmarshaller;
 
 final class JaxbContextContainer {
 
-    private static final String PACKAGE_NODE_FOR_PSP = "it.pagopa.generated.transactions.model";
+  private static final String PACKAGE_NODE =
+      "it.pagopa.generated.transactions.model:it.pagopa.generated.nodoperpsp.model";
 
-    private static JAXBContext jaxbContext = null;
+  private static JAXBContext jaxbContext = null;
 
-    public Marshaller createMarshaller() throws JAXBException {
-        return getJaxbContext().createMarshaller();
-    }
+  public Marshaller createMarshaller() throws JAXBException {
+    return getJaxbContext().createMarshaller();
+  }
 
-    public Unmarshaller createUnmarshaller() throws JAXBException {
-        return getJaxbContext().createUnmarshaller();
-    }
+  public Unmarshaller createUnmarshaller() throws JAXBException {
+    return getJaxbContext().createUnmarshaller();
+  }
 
-    private JAXBContext getJaxbContext() throws JAXBException {
-        return jaxbContext  == null ? JAXBContext.newInstance(PACKAGE_NODE_FOR_PSP) : jaxbContext;
-    }
-
+  private JAXBContext getJaxbContext() throws JAXBException {
+    return jaxbContext == null ? JAXBContext.newInstance(PACKAGE_NODE) : jaxbContext;
+  }
 }
