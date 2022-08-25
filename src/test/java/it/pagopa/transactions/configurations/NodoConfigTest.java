d383c99c811e9e5bf8708573cc38c22d020c20fd
Simone Infante <simone.infante@pagopa.it>
2022-08-25 14:06:21 +0200

    fix(test): code smell

--- src/test/java/it/pagopa/transactions/configurations/NodoConfigTest.java
+++ src/test/java/it/pagopa/transactions/configurations/NodoConfigTest.java
@@ -4,21 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import it.pagopa.generated.nodoperpsp.model.NodoVerificaRPT;
 import it.pagopa.generated.transactions.model.ObjectFactory;
 import it.pagopa.generated.transactions.model.VerifyPaymentNoticeReq;
-import it.pagopa.transactions.utils.soap.Jaxb2SoapEncoder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.xml.bind.Marshaller;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
-public class NodoConfigTest {
+class NodoConfigTest {
 
   @InjectMocks private NodoConfig nodoConfig;
 
@@ -33,7 +28,7 @@ public class NodoConfigTest {
     NodoVerificaRPT nodoVerificaRPT =
         nodoConfig.baseNodoVerificaRPTRequest(
             nodoConnectionString, new it.pagopa.generated.nodoperpsp.model.ObjectFactory());
-    assertEquals(nodoVerificaRPT != null, Boolean.TRUE);
+    assertEquals(Boolean.TRUE, nodoVerificaRPT != null);
   }
 
   @Test
@@ -43,6 +38,6 @@ public class NodoConfigTest {
 
     VerifyPaymentNoticeReq verifyPaymentNoticeReq =
         nodoConfig.baseVerifyPaymentNoticeReq(nodoConnectionString, new ObjectFactory());
-    assertEquals(verifyPaymentNoticeReq != null, Boolean.TRUE);
+    assertEquals(Boolean.TRUE, verifyPaymentNoticeReq != null);
   }
 }
