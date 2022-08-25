d2e3c3208a031aa33b932044c204c1a8f3fa6185
Simone Infante <simone.infante@pagopa.it>
2022-08-25 14:33:46 +0200

    feat(test): add test for dueDate

--- src/test/java/it/pagopa/transactions/services/PaymentRequestsServiceTest.java
+++ src/test/java/it/pagopa/transactions/services/PaymentRequestsServiceTest.java
@@ -2,10 +2,13 @@ package it.pagopa.transactions.services;
 
 import it.pagopa.generated.nodoperpsp.model.*;
 import it.pagopa.generated.payment.requests.model.PaymentRequestsGetResponseDto;
+import it.pagopa.generated.payment.requests.model.PaymentStatusFaultDto;
+import it.pagopa.generated.payment.requests.model.ValidationFaultDto;
 import it.pagopa.generated.transactions.model.*;
 import it.pagopa.transactions.client.NodeForPspClient;
 import it.pagopa.transactions.client.NodoPerPspClient;
 import it.pagopa.transactions.domain.RptId;
+import it.pagopa.transactions.exceptions.NodoErrorException;
 import it.pagopa.transactions.repositories.PaymentRequestInfo;
 import it.pagopa.transactions.repositories.PaymentRequestsInfoRepository;
 import org.junit.jupiter.api.Test;
@@ -16,9 +19,18 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
@@ -119,7 +131,7 @@ class PaymentRequestsServiceTest {
     final BigDecimal amount = BigDecimal.valueOf(1000);
 
     PaymentRequestInfo paymentRequestInfo =
-            new PaymentRequestInfo(rptIdAsObject, paTaxCode, paName, description, amount, null, true);
+        new PaymentRequestInfo(rptIdAsObject, paTaxCode, paName, description, amount, null, true);
 
     NodoVerificaRPTRisposta verificaRPTRIsposta = new NodoVerificaRPTRisposta();
     EsitoNodoVerificaRPTRisposta esitoVerificaRPT = new EsitoNodoVerificaRPTRisposta();
@@ -140,21 +152,126 @@ class PaymentRequestsServiceTest {
 
     /** Preconditions */
     Mockito.when(paymentRequestsInfoRepository.findById(rptIdAsObject))
-            .thenReturn(Optional.empty());
+        .thenReturn(Optional.empty());
     Mockito.when(objectFactoryNodoPerPsp.createNodoTipoCodiceIdRPT())
-            .thenReturn(new NodoTipoCodiceIdRPT());
+        .thenReturn(new NodoTipoCodiceIdRPT());
     Mockito.when(nodoPerPspClient.verificaRPT(Mockito.any()))
-            .thenReturn(Mono.just(verificaRPTRIsposta));
+        .thenReturn(Mono.just(verificaRPTRIsposta));
     Mockito.when(nodeForPspClient.verifyPaymentNotice(Mockito.any()))
-            .thenReturn(Mono.just(verifyPaymentNotice));
+        .thenReturn(Mono.just(verifyPaymentNotice));
     /** Test */
     PaymentRequestsGetResponseDto responseDto =
-            paymentRequestsService.getPaymentRequestInfo(rptIdAsString).block();
+        paymentRequestsService.getPaymentRequestInfo(rptIdAsString).block();
 
     /** Assertions */
     assertEquals(responseDto.getRptId(), rptIdAsString);
     assertEquals(responseDto.getDescription(), description);
     assertEquals(responseDto.getDueDate(), null);
+    assertEquals(BigDecimal.valueOf(responseDto.getAmount()), amount);
+  }
+
+  @Test
+  void shouldReturnPaymentOngoingFromNodoVerificaRPT() {
+    final String rptIdAsString = "77777777777302016723749670035";
+    final RptId rptIdAsObject = new RptId(rptIdAsString);
+
+    NodoVerificaRPTRisposta verificaRPTRIsposta = new NodoVerificaRPTRisposta();
+    EsitoNodoVerificaRPTRisposta esitoVerificaRPT = new EsitoNodoVerificaRPTRisposta();
+    esitoVerificaRPT.setEsito(StOutcome.KO.value());
+    FaultBean fault = new FaultBean();
+    fault.setFaultCode(PaymentStatusFaultDto.PPT_PAGAMENTO_IN_CORSO.getValue());
+    esitoVerificaRPT.setFault(fault);
+    verificaRPTRIsposta.setNodoVerificaRPTRisposta(esitoVerificaRPT);
+
+    /** Preconditions */
+    Mockito.when(paymentRequestsInfoRepository.findById(rptIdAsObject))
+        .thenReturn(Optional.empty());
+    Mockito.when(objectFactoryNodoPerPsp.createNodoTipoCodiceIdRPT())
+        .thenReturn(new NodoTipoCodiceIdRPT());
+    Mockito.when(nodoPerPspClient.verificaRPT(Mockito.any()))
+        .thenReturn(Mono.just(verificaRPTRIsposta));
+
+    /** Test */
+    assertThatThrownBy(() -> paymentRequestsService.getPaymentRequestInfo(rptIdAsString).block())
+        .isInstanceOf(NodoErrorException.class);
+  }
+
+  @Test
+  void shouldReturnPaymentUnknowFromNodoVerificaRPT() {
+    final String rptIdAsString = "77777777777302016723749670035";
+    final RptId rptIdAsObject = new RptId(rptIdAsString);
+
+    NodoVerificaRPTRisposta verificaRPTRIsposta = new NodoVerificaRPTRisposta();
+    EsitoNodoVerificaRPTRisposta esitoVerificaRPT = new EsitoNodoVerificaRPTRisposta();
+    esitoVerificaRPT.setEsito(StOutcome.KO.value());
+    FaultBean fault = new FaultBean();
+    fault.setFaultCode(ValidationFaultDto.PPT_DOMINIO_SCONOSCIUTO.getValue());
+    esitoVerificaRPT.setFault(fault);
+    verificaRPTRIsposta.setNodoVerificaRPTRisposta(esitoVerificaRPT);
+
+    /** Preconditions */
+    Mockito.when(paymentRequestsInfoRepository.findById(rptIdAsObject))
+        .thenReturn(Optional.empty());
+    Mockito.when(objectFactoryNodoPerPsp.createNodoTipoCodiceIdRPT())
+        .thenReturn(new NodoTipoCodiceIdRPT());
+    Mockito.when(nodoPerPspClient.verificaRPT(Mockito.any()))
+        .thenReturn(Mono.just(verificaRPTRIsposta));
+
+    /** Test */
+    assertThatThrownBy(() -> paymentRequestsService.getPaymentRequestInfo(rptIdAsString).block())
+        .isInstanceOf(NodoErrorException.class);
+  }
+
+  @Test
+  void shouldReturnPaymentInfoRequestFromNodoVerifyPaymentNoticeWithDueDate()
+      throws ParseException, DatatypeConfigurationException {
+    final String rptIdAsString = "77777777777302016723749670035";
+    final RptId rptIdAsObject = new RptId(rptIdAsString);
+    final String paTaxCode = "77777777777";
+    final String paName = "Pa Name";
+    final String description = "Payment request description";
+    final BigDecimal amount = BigDecimal.valueOf(1000);
+
+    DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    Date date = format.parse("2022-04-24");
+    XMLGregorianCalendar dueDate =
+        DatatypeFactory.newInstance().newXMLGregorianCalendar(format.format(date));
+
+    NodoVerificaRPTRisposta verificaRPTRIsposta = new NodoVerificaRPTRisposta();
+    EsitoNodoVerificaRPTRisposta esitoVerificaRPT = new EsitoNodoVerificaRPTRisposta();
+    esitoVerificaRPT.setEsito(StOutcome.KO.value());
+    FaultBean fault = new FaultBean();
+    fault.setFaultCode("PPT_MULTI_BENEFICIARIO");
+    esitoVerificaRPT.setFault(fault);
+    verificaRPTRIsposta.setNodoVerificaRPTRisposta(esitoVerificaRPT);
+
+    VerifyPaymentNoticeRes verifyPaymentNotice = new VerifyPaymentNoticeRes();
+    verifyPaymentNotice.setOutcome(StOutcome.OK);
+    verifyPaymentNotice.setPaymentDescription(description);
+    CtPaymentOptionsDescriptionList paymentList = new CtPaymentOptionsDescriptionList();
+    CtPaymentOptionDescription paymentDescription = new CtPaymentOptionDescription();
+    paymentDescription.setAmount(amount);
+    paymentDescription.setDueDate(dueDate);
+    paymentList.getPaymentOptionDescription().add(paymentDescription);
+    verifyPaymentNotice.setPaymentList(paymentList);
+
+    /** Preconditions */
+    Mockito.when(paymentRequestsInfoRepository.findById(rptIdAsObject))
+        .thenReturn(Optional.empty());
+    Mockito.when(objectFactoryNodoPerPsp.createNodoTipoCodiceIdRPT())
+        .thenReturn(new NodoTipoCodiceIdRPT());
+    Mockito.when(nodoPerPspClient.verificaRPT(Mockito.any()))
+        .thenReturn(Mono.just(verificaRPTRIsposta));
+    Mockito.when(nodeForPspClient.verifyPaymentNotice(Mockito.any()))
+        .thenReturn(Mono.just(verifyPaymentNotice));
+    /** Test */
+    PaymentRequestsGetResponseDto responseDto =
+        paymentRequestsService.getPaymentRequestInfo(rptIdAsString).block();
+
+    /** Assertions */
+    assertEquals(responseDto.getRptId(), rptIdAsString);
+    assertEquals(responseDto.getDescription(), description);
+    assertEquals("2022-04-24", responseDto.getDueDate());
     assertEquals(BigDecimal.valueOf(responseDto.getAmount()), amount);
   }
 }
