e5ef29c7c58ec4007826216e5bc77a7c212c3735
Simone Infante <simone.infante@pagopa.it>
2022-09-02 16:31:43 +0200

    feat(attivarpt): add nodo attivaRPT in nodo client

--- src/main/java/it/pagopa/transactions/configurations/NodoConfig.java
+++ src/main/java/it/pagopa/transactions/configurations/NodoConfig.java
@@ -1,6 +1,8 @@
 package it.pagopa.transactions.configurations;
 
+import it.pagopa.generated.nodoperpsp.model.NodoAttivaRPT;
 import it.pagopa.generated.nodoperpsp.model.NodoVerificaRPT;
+import it.pagopa.generated.transactions.model.ActivatePaymentNoticeReq;
 import it.pagopa.generated.transactions.model.VerifyPaymentNoticeReq;
 import it.pagopa.transactions.utils.NodoConnectionString;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +40,22 @@ public class NodoConfig {
   }
 
   @Bean
+  public NodoAttivaRPT baseNodoAttivaRPTRequest(
+          @Value("${nodo.connection.string}") String nodoConnectionParamsAsString,
+          it.pagopa.generated.nodoperpsp.model.ObjectFactory objectFactoryNodoPerPsp)
+          throws JsonProcessingException {
+
+    NodoConnectionString nodoConnectionParams = nodoConnectionString(nodoConnectionParamsAsString);
+
+    NodoAttivaRPT request = objectFactoryNodoPerPsp.createNodoAttivaRPT();
+    request.setIdentificativoPSP(nodoConnectionParams.getIdPSP());
+    request.setIdentificativoCanale(nodoConnectionParams.getIdChannel());
+    request.setIdentificativoIntermediarioPSP(nodoConnectionParams.getIdBrokerPSP());
+    request.setPassword(nodoConnectionParams.getPassword());
+    return request;
+  }
+
+  @Bean
   public VerifyPaymentNoticeReq baseVerifyPaymentNoticeReq(
       @Value("${nodo.connection.string}") String nodoConnectionParamsAsString,
       it.pagopa.generated.transactions.model.ObjectFactory objectFactoryNodeForPsp)
@@ -50,6 +68,23 @@ public class NodoConfig {
     request.setIdChannel(nodoConnectionParams.getIdChannel());
     request.setIdBrokerPSP(nodoConnectionParams.getIdBrokerPSP());
     request.setPassword(nodoConnectionParams.getPassword());
+    return request;
+  }
+
+  @Bean
+  public ActivatePaymentNoticeReq baseActivatePaymentNoticeReq(
+      @Value("${nodo.connection.string}") String nodoConnectionParamsAsString,
+      it.pagopa.generated.transactions.model.ObjectFactory objectFactoryNodeForPsp)
+      throws JsonProcessingException {
+
+    NodoConnectionString nodoConnectionParams = nodoConnectionString(nodoConnectionParamsAsString);
+
+    ActivatePaymentNoticeReq request = objectFactoryNodeForPsp.createActivatePaymentNoticeReq();
+    request.setIdPSP(nodoConnectionParams.getIdPSP());
+    request.setIdChannel(nodoConnectionParams.getIdChannel());
+    request.setIdBrokerPSP(nodoConnectionParams.getIdBrokerPSP());
+    request.setPassword(nodoConnectionParams.getPassword());
+
     return request;
   }
 }
