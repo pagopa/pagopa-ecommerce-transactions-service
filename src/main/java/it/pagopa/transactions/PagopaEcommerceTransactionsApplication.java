5dd676068e4af4f8f9d470f7d4f91afeb8a27695
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.station>
2022-10-26 17:52:24 +0200

    add MDC utilities to track request and thread

--- src/main/java/it/pagopa/transactions/PagopaEcommerceTransactionsApplication.java
+++ src/main/java/it/pagopa/transactions/PagopaEcommerceTransactionsApplication.java
@@ -12,33 +12,10 @@ import java.util.Map;
 import java.util.concurrent.Executor;
 @EnableAsync(proxyTargetClass = true)
 @SpringBootApplication
-public class PagopaEcommerceTransactionsApplication  extends AsyncConfigurerSupport {
-
-    @Override
-    public Executor getAsyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setTaskDecorator(new MdcTaskDecorator());
-        executor.initialize();
-        return executor;
-    }
+public class PagopaEcommerceTransactionsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PagopaEcommerceTransactionsApplication.class, args);
     }
 
-}
-
-class MdcTaskDecorator  implements TaskDecorator {
-    @Override
-    public Runnable decorate(Runnable runnable) {
-        Map<String, String> contextMap = MDC.getCopyOfContextMap();
-        return () -> {
-            try {
-                MDC.setContextMap(contextMap);
-                runnable.run();
-            } finally {
-                MDC.clear();
-            }
-        };
-    }
 }
--- src/main/java/it/pagopa/transactions/mdcutilities/MDCFilter.java
+++ src/main/java/it/pagopa/transactions/mdcutilities/MDCFilter.java
@@ -2,6 +2,7 @@ package it.pagopa.transactions.mdcutilities;
 
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -10,9 +11,12 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import org.springframework.web.util.UriTemplate;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Signal;
 
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 @Component
 @Slf4j
@@ -36,16 +40,38 @@ public class MDCFilter implements WebFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         Map<String,String> transactionMap = getTransactionId(exchange.getRequest());
-        MDC.put("diagnosticContextKey", UUID.randomUUID().toString());
-        MDC.put("transactionId", transactionMap.getOrDefault("transactionId",""));
-        MDC.put("paymentContextCode", transactionMap.getOrDefault("paymentContextCode",""));
+        //MDC.put("diagnosticContextKey", UUID.randomUUID().toString());
+        //MDC.put("transactionId", transactionMap.getOrDefault("transactionId",""));
+        //MDC.put("paymentContextCode", transactionMap.getOrDefault("paymentContextCode",""));
         return chain.filter(decorate(exchange));
+        //.doOnEach(logOnEach(r -> log.info("{} {} {}", request.getMethod(), request.getURI(), request.getBody())));
+        //.contextWrite(Context.of("CONTEXT_KEY", requestId))
+        //.contextWrite(Context.of("TRANSACTION_ID", transactionMap.getOrDefault("transactionId", "")));
     }
 
     private Map<String, String> getTransactionId(ServerHttpRequest request) {
         UriTemplate uriTemplatePCC = new UriTemplate("/transactions/payment-context-codes/{paymentContextCode}/activation-results");
         UriTemplate uriTemplateStandard = new UriTemplate("/transactions/{transactionId}");
         return uriTemplatePCC.matches(request.getPath().value()) ? uriTemplatePCC.match(request.getPath().value()) : uriTemplateStandard.match(request.getPath().value());
+    }
+
+    public static <T> Consumer<Signal<T>> logOnEach(Consumer<T> logStatement) {
+        return signal -> {
+            String contextValue = signal.getContextView().get("CONTEXT_KEY");
+            try (MDC.MDCCloseable cMdc = MDC.putCloseable("MDC_KEY", contextValue)) {
+                logStatement.accept(signal.get());
+            }
+        };
+    }
+
+    public static <T> Consumer<Signal<T>> logOnNext(Consumer<T> logStatement) {
+        return signal -> {
+            if (!signal.isOnNext()) return;
+            String contextValue = signal.getContextView().get("CONTEXT_KEY");
+            try (MDC.MDCCloseable cMdc = MDC.putCloseable("MDC_KEY", contextValue)) {
+                logStatement.accept(signal.get());
+            }
+        };
     }
 
 }
