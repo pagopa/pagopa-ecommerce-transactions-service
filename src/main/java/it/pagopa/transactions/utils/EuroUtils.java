cbeafd521cda01726a349df9bef0fa7d06610c00
Simone Infante <simone.infante@pagopa.it>
2022-10-27 16:43:10 +0200

    fix: add private constructor for euroutils

--- src/main/java/it/pagopa/transactions/utils/EuroUtils.java
+++ src/main/java/it/pagopa/transactions/utils/EuroUtils.java
@@ -5,6 +5,9 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class EuroUtils {
+	private EuroUtils() {
+		throw new IllegalStateException("Utility EuroUtils class");
+	}
 
 	public static BigDecimal euroCentsToEuro(Integer euroCents){
 		return BigDecimal.valueOf(euroCents).divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP);
--- src/test/java/it/pagopa/transactions/commands/handlers/TransactionSendClosureHandlerTest.java
+++ src/test/java/it/pagopa/transactions/commands/handlers/TransactionSendClosureHandlerTest.java
@@ -25,8 +25,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Map;
