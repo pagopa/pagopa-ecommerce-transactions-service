3f9e011e2d25397ec6034351ca96b53a3961b173
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.station>
2022-10-28 08:52:52 +0200

    exclude file for coverage

--- src/main/java/it/pagopa/transactions/PagopaEcommerceTransactionsApplication.java
+++ src/main/java/it/pagopa/transactions/PagopaEcommerceTransactionsApplication.java
@@ -1,6 +1,5 @@
 package it.pagopa.transactions;
 
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
