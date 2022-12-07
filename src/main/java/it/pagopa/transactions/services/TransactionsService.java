485ad0107eca48cbf79c7104e115b08405a0bea1
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.station>
2022-12-07 16:53:47 +0100

    fix parameter name

--- src/main/java/it/pagopa/transactions/services/TransactionsService.java
+++ src/main/java/it/pagopa/transactions/services/TransactionsService.java
@@ -112,7 +112,7 @@ public class TransactionsService {
   }
 
   public Mono<RequestAuthorizationResponseDto> requestTransactionAuthorization(
-      String transactionId, String pgsId, RequestAuthorizationRequestDto requestAuthorizationRequestDto) {
+      String transactionId, String paymentGatewayId, RequestAuthorizationRequestDto requestAuthorizationRequestDto) {
     return transactionsViewRepository
         .findById(transactionId)
         .switchIfEmpty(Mono.error(new TransactionNotFoundException(transactionId)))
@@ -189,7 +189,7 @@ public class TransactionsService {
                       psp.getChannelCode(),
                       paymentMethod.getName(),
                       psp.getBusinessName(),
-                          pgsId,
+                          paymentGatewayId,
                           null,
                           null,
                           null);
