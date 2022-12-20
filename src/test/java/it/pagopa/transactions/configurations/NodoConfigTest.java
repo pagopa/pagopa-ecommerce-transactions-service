d7b5dd22ff6299484cf459ebc4825c62212499be
Pietro Tota <pietro.tota@pagopa.it>
2022-12-20 15:37:47 +0100

    fix: nodo request building side-effect

--- src/test/java/it/pagopa/transactions/configurations/NodoConfigTest.java
+++ src/test/java/it/pagopa/transactions/configurations/NodoConfigTest.java
@@ -1,14 +1,11 @@
 package it.pagopa.transactions.configurations;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import it.pagopa.generated.nodoperpsp.model.NodoVerificaRPT;
-import it.pagopa.generated.transactions.model.ObjectFactory;
 import it.pagopa.generated.transactions.model.VerifyPaymentNoticeReq;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import java.lang.reflect.InvocationTargetException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,27 +15,16 @@ class NodoConfigTest {
     @InjectMocks
     private NodoConfig nodoConfig;
 
-    private final String nodoConnectionString = "{\"idPSP\":\"idPsp\",\"idChannel\":\"idChannel\",\"idBrokerPSP\":\"idBrokerPsp\",\"password\":\"password\"}";
-
     @Test
-    void shouldReturnValidVerificaRPTBaseRequest()
-            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException,
-            JsonProcessingException {
-
-        NodoVerificaRPT nodoVerificaRPT = nodoConfig.baseNodoVerificaRPTRequest(
-                nodoConnectionString,
-                new it.pagopa.generated.nodoperpsp.model.ObjectFactory()
-        );
+    void shouldReturnValidVerificaRPTBaseRequest() {
+        NodoVerificaRPT nodoVerificaRPT = nodoConfig.baseNodoVerificaRPTRequest();
         assertEquals(Boolean.TRUE, nodoVerificaRPT != null);
     }
 
     @Test
-    void shouldReturnValidVerifyPaymentNoticeBaseRequest()
-            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException,
-            JsonProcessingException {
-
+    void shouldReturnValidVerifyPaymentNoticeBaseRequest() {
         VerifyPaymentNoticeReq verifyPaymentNoticeReq = nodoConfig
-                .baseVerifyPaymentNoticeReq(nodoConnectionString, new ObjectFactory());
+                .baseVerifyPaymentNoticeReq();
         assertEquals(Boolean.TRUE, verifyPaymentNoticeReq != null);
     }
 }
