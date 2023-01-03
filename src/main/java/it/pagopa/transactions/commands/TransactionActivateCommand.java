d609e2bb0d59cdb60f80104745ce936eafceb97e
Pietro Tota <pietro.tota@pagopa.it>
2023-01-03 11:19:50 +0100

    fix: sonar cube issues

--- src/main/java/it/pagopa/transactions/commands/TransactionActivateCommand.java
+++ src/main/java/it/pagopa/transactions/commands/TransactionActivateCommand.java
@@ -3,7 +3,11 @@ package it.pagopa.transactions.commands;
 import it.pagopa.ecommerce.commons.documents.Transaction;
 import it.pagopa.ecommerce.commons.domain.RptId;
 import it.pagopa.generated.transactions.server.model.NewTransactionRequestDto;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
+@Getter
+@EqualsAndHashCode(callSuper = true)
 public final class TransactionActivateCommand extends TransactionsCommand<NewTransactionRequestDto> {
 
     private Transaction.OriginType originType;
@@ -17,22 +21,4 @@ public final class TransactionActivateCommand extends TransactionsCommand<NewTra
         this.originType = originType;
     }
 
-    public TransactionActivateCommand(
-            RptId rptId,
-            NewTransactionRequestDto data,
-            String originType
-    ) {
-        this(
-                rptId,
-                data,
-                Transaction.OriginType.fromString(
-                        originType
-
-                )
-        );
-    }
-
-    public Transaction.OriginType getOriginType() {
-        return originType;
-    }
 }
