543871c7528f7a0fe2bf439aa334d2d9a0ff2b4d
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-03-01 10:05:20 +0100

    fix test

--- src/test/java/it/pagopa/transactions/utils/LogMaskPatternTests.java
+++ src/test/java/it/pagopa/transactions/utils/LogMaskPatternTests.java
@@ -3,9 +3,7 @@ package it.pagopa.transactions.utils;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.LoggingEvent;
-import ch.qos.logback.core.ContextBase;
 import org.junit.Test;
-import org.mockito.Mock;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,32 +11,48 @@ public class LogMaskPatternTests {
 
     @Test
     public void testPatternMatch() {
+        LoggerContext loggerContext = new LoggerContext();
         LogMaskerPatternLayout layout = new LogMaskerPatternLayout();
-        layout.setPattern("test 1234 abcd 1234 test");
+        layout.setPattern("%d{HH:mm:ss.SSS} %-5level %logger{36} - %msg%n");
         layout.addMaskPattern("(abcd)");
-        layout.setContext(new LoggerContext());
+        layout.setContext(loggerContext);
         layout.start();
-        LoggingEvent event = new LoggingEvent();
-        event.setLevel(Level.INFO);
-        event.setMessage("Message test");
+        LoggingEvent event = new LoggingEvent(
+                "test",
+                loggerContext.getLogger("ROOT"),
+                Level.INFO,
+                "test 1234 abcd 1234 test",
+                null,
+                null
+        );
         String maskedMessage = layout.doLayout(event);
         layout.stop();
-        assertEquals("test 1234 **** 1234 test", maskedMessage);
+        assertEquals("test 1234 **** 1234 test", extractMaskedMessage(maskedMessage));
     }
 
     @Test
     public void testPatternNotMatch() {
+        LoggerContext loggerContext = new LoggerContext();
         LogMaskerPatternLayout layout = new LogMaskerPatternLayout();
-        layout.setPattern("test 1234 test");
+        layout.setPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n");
         layout.addMaskPattern("(abcd)");
-        layout.setContext(new LoggerContext());
+        layout.setContext(loggerContext);
         layout.start();
-        LoggingEvent event = new LoggingEvent();
-        event.setLevel(Level.INFO);
-        event.setMessage("Message test");
+        LoggingEvent event = new LoggingEvent(
+                "test",
+                loggerContext.getLogger("ROOT"),
+                Level.INFO,
+                "test 1234 test",
+                null,
+                null
+        );
         String maskedMessage = layout.doLayout(event);
         layout.stop();
-        assertEquals("test 1234 test", maskedMessage);
+        assertEquals("test 1234 test", extractMaskedMessage(maskedMessage));
+    }
+
+    private String extractMaskedMessage(String maskedMessage) {
+        return maskedMessage.split(" - ")[1].trim();
     }
 
 }
