1550f1fdd5aa05241ed9139a5a3c0174eb609a24
Fernando-Granato <fernando.granato@pagopa.it>
2023-03-16 11:07:19 +0100

    feat: added CancellationRequestProjectionHandler and CancellationRequestProjectionHandlerTest

---
+++ src/main/java/it/pagopa/transactions/projections/handlers/CancellationRequestProjectionHandler.java
@@ -0,0 +1,35 @@
+package it.pagopa.transactions.projections.handlers;
+
+import it.pagopa.ecommerce.commons.documents.v1.Transaction;
+import it.pagopa.ecommerce.commons.documents.v1.TransactionUserCanceledEvent;
+import it.pagopa.ecommerce.commons.generated.server.model.TransactionStatusDto;
+import it.pagopa.transactions.exceptions.TransactionNotFoundException;
+import it.pagopa.transactions.repositories.TransactionsViewRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class CancellationRequestProjectionHandler implements ProjectionHandler<TransactionUserCanceledEvent, Mono<Transaction>> {
+
+    @Autowired
+    private TransactionsViewRepository transactionsViewRepository;
+
+    @Override
+    public Mono<Transaction> handle(TransactionUserCanceledEvent transactionUserCanceledEvent) {
+        return transactionsViewRepository.findById(transactionUserCanceledEvent.getTransactionId())
+                .switchIfEmpty(
+                        Mono.error(
+                                new TransactionNotFoundException(
+                                        transactionUserCanceledEvent.getTransactionId()
+                                )
+                        )
+                )
+                .flatMap(transactionDocument -> {
+                    transactionDocument.setStatus(TransactionStatusDto.CANCELLATION_REQUESTED);
+                    return transactionsViewRepository.save(transactionDocument);
+                });
+    }
+}
