18c7a510ebe5417f799d96372a57323c1de2bd10
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-04-14 13:09:54 +0200

    spotless apply

--- src/test/java/it/pagopa/transactions/utils/NodoOperationsTest.java
+++ src/test/java/it/pagopa/transactions/utils/NodoOperationsTest.java
@@ -96,9 +96,9 @@ class NodoOperationsTest {
         Mockito.when(nodeForPspClient.activatePaymentNoticeV2(Mockito.any()))
                 .thenReturn(Mono.just(activatePaymentRes));
         Mockito.when(
-                        objectFactoryNodeForPsp
-                                .createActivatePaymentNoticeV2Request(argThat(req -> req.getPaymentNote().equals(idCart)))
-                )
+                objectFactoryNodeForPsp
+                        .createActivatePaymentNoticeV2Request(argThat(req -> req.getPaymentNote().equals(idCart)))
+        )
                 .thenAnswer(args -> objectFactoryUtil.createActivatePaymentNoticeV2Request(args.getArgument(0)));
         Mockito.when(nodoConfig.baseActivatePaymentNoticeV2Request()).thenReturn(new ActivatePaymentNoticeV2Request());
 
@@ -180,9 +180,9 @@ class NodoOperationsTest {
         Mockito.when(nodeForPspClient.activatePaymentNoticeV2(Mockito.any()))
                 .thenReturn(Mono.just(activatePaymentRes));
         Mockito.when(
-                        objectFactoryNodeForPsp
-                                .createActivatePaymentNoticeV2Request(argThat(req -> req.getPaymentNote() == null))
-                )
+                objectFactoryNodeForPsp
+                        .createActivatePaymentNoticeV2Request(argThat(req -> req.getPaymentNote() == null))
+        )
                 .thenAnswer(args -> objectFactoryUtil.createActivatePaymentNoticeV2Request(args.getArgument(0)));
         Mockito.when(nodoConfig.baseActivatePaymentNoticeV2Request()).thenReturn(new ActivatePaymentNoticeV2Request());
 
