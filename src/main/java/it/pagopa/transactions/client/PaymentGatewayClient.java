478e622f52f60c3166dc8fd01dbe5db18e773eea
Simone Infante <simone.infante@pagopa.it>
2023-04-27 12:13:56 +0200

    fix: typo

--- src/main/java/it/pagopa/transactions/client/PaymentGatewayClient.java
+++ src/main/java/it/pagopa/transactions/client/PaymentGatewayClient.java
@@ -206,7 +206,7 @@ public class PaymentGatewayClient {
                 })
                 .flatMap(
                         creditCardAuthRequestDto -> creditCardInternalApiClient
-                                .step0VposAuthAuth(
+                                .step0VposAuth(
                                         creditCardAuthRequestDto,
                                         encodeMdcFields(authorizationData)
                                 )
