b3485e32ecff62c95aa6e10a4aa74db00a46f532
Marco Greco <marcoandreagreco@gmail.com>
2023-05-11 10:16:58 +0200

    [CHK-1268][CHK-1488] feat: improve circuit-breakers logging (#212)
    * [CHK-1268] feat: improve circuit-breakers logging
    
    * [CHK-1268] fix: fmt
    
    * [CHK-1268] fix: minor
    
    * [CHK-1268] fix: minor
    
    * [CHK-1268] fix: circuit breakers events
    
    * [CHK-1268] fix: test circuit breakers events
    
    * [CHK-1268] fix: fmt
    
    * [CHK-1488] fix: optimize imports
    
    * [CHK-1488] fix: minor changes
    
    * [CHK-1488] fix: minor changes
    
    * [CHK-1488] fix: tests
    
    * [CHK-1488] fix: fmt

--- src/main/java/it/pagopa/transactions/PagopaEcommerceTransactionsApplication.java
+++ src/main/java/it/pagopa/transactions/PagopaEcommerceTransactionsApplication.java
@@ -1,8 +1,17 @@
 package it.pagopa.transactions;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.common.circuitbreaker.configuration.CircuitBreakerConfigCustomizer;
+import io.github.resilience4j.core.registry.EntryAddedEvent;
+import io.github.resilience4j.core.registry.EntryRemovedEvent;
+import io.github.resilience4j.core.registry.EntryReplacedEvent;
+import io.github.resilience4j.core.registry.RegistryEventConsumer;
+import io.github.resilience4j.retry.annotation.Retry;
 import it.pagopa.ecommerce.commons.ConfigScan;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,10 +20,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableRedisRepositories(basePackages = "it.pagopa.ecommerce.commons.repositories")
 @Import(ConfigScan.class)
+@Slf4j
 public class PagopaEcommerceTransactionsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PagopaEcommerceTransactionsApplication.class, args);
     }
-
 }
