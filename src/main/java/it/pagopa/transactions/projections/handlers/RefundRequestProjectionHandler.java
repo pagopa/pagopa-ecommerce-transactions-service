1e554af1e340c2951cea175fb7cde71e0d4bb76b
Fernando-Granato <fernando.granato@pagopa.it>
2023-05-24 11:55:16 +0200

    fix: added Component annotation into RefundRequestProjectionHandler

--- src/main/java/it/pagopa/transactions/projections/handlers/RefundRequestProjectionHandler.java
+++ src/main/java/it/pagopa/transactions/projections/handlers/RefundRequestProjectionHandler.java
@@ -2,13 +2,16 @@ package it.pagopa.transactions.projections.handlers;
 
 import it.pagopa.ecommerce.commons.documents.v1.Transaction;
 import it.pagopa.ecommerce.commons.documents.v1.TransactionRefundRequestedEvent;
-import it.pagopa.ecommerce.commons.documents.v1.TransactionUserCanceledEvent;
 import it.pagopa.ecommerce.commons.generated.server.model.TransactionStatusDto;
 import it.pagopa.transactions.exceptions.TransactionNotFoundException;
 import it.pagopa.transactions.repositories.TransactionsViewRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+@Component
+@Slf4j
 public class RefundRequestProjectionHandler
         implements ProjectionHandler<TransactionRefundRequestedEvent, Mono<Transaction>> {
 
