abfd31ae10d6f66ad3ebe4a22a3d69f17066a355
Simone Infante <simone.infante@pagopa.it>
2023-05-31 10:57:03 +0200

    fix: lint

--- src/test/java/it/pagopa/transactions/utils/AuthRequestDataUtilsTest.java
+++ src/test/java/it/pagopa/transactions/utils/AuthRequestDataUtilsTest.java
@@ -113,7 +113,7 @@ class AuthRequestDataUtilsTest {
         AuthRequestDataUtils.AuthRequestData data = authRequestDataUtils
                 .from(updateAuthorizationRequest, transactionId);
 
-        assertEquals(data.rrn(), transactionId.value().substring(0,7));
+        assertEquals(data.rrn(), transactionId.value().substring(0, 7));
         assertEquals(
                 data.authorizationCode(),
                 ((OutcomeXpayGatewayDto) updateAuthorizationRequest.getOutcomeGateway()).getAuthorizationCode()
