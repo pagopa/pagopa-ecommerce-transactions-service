865f7d5814678e8db01b4db0a87715fc52d316c3
Pietro Tota <pietro.tota@pagopa.it>
2023-05-31 16:10:03 +0200

    fix: PaymentRequestInfoTTL

--- src/main/java/it/pagopa/transactions/configurations/RedisConfig.java
+++ src/main/java/it/pagopa/transactions/configurations/RedisConfig.java
@@ -2,6 +2,7 @@ package it.pagopa.transactions.configurations;
 
 import it.pagopa.ecommerce.commons.redis.RedisTemplateWrapperBuilder;
 import it.pagopa.ecommerce.commons.redis.templatewrappers.PaymentRequestInfoRedisTemplateWrapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,11 +14,16 @@ public class RedisConfig {
 
     @Bean
     public PaymentRequestInfoRedisTemplateWrapper paymentRequestInfoWrapper(
-                                                                            RedisConnectionFactory redisConnectionFactory
+                                                                            RedisConnectionFactory redisConnectionFactory,
+                                                                            @Value(
+                                                                                "${payment.token.validity}"
+                                                                            ) Integer paymentTokenTimeout
     ) {
+        // PaymentRequestInfo entities will have the same TTL as paymentTokenTimeout
+        // value
         return RedisTemplateWrapperBuilder.buildPaymentRequestInfoRedisTemplateWrapper(
                 redisConnectionFactory,
-                Duration.ofMinutes(10)
+                Duration.ofSeconds(paymentTokenTimeout)
         );
     }
 }
