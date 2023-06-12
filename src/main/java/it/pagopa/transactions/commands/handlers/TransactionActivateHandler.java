bf2d8a9fb0d50d3864ee7370b04a89781aa82170
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-06-12 12:29:32 +0200

    use null io place of false hardwired value in activation when an old idempotence key where found

--- src/main/java/it/pagopa/transactions/commands/handlers/TransactionActivateHandler.java
+++ src/main/java/it/pagopa/transactions/commands/handlers/TransactionActivateHandler.java
@@ -135,7 +135,7 @@ public class TransactionActivateHandler
                                                                                                 .generateRandomStringToIdempotencyKey()
                                                                                 ),
                                                                                 new ArrayList<>(TRANSFER_LIST_MAX_SIZE),
-                                                                                false
+                                                                                null
                                                                         );
                                                                         paymentRequestInfoRedisTemplateWrapper
                                                                                 .save(
