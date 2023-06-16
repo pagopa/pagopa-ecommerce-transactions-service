1d881efd73ecd895ee4bd5b93b321dc2e92fe082
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2023-06-16 13:06:16 +0200

    feat(view): sendPaymentResult outcome NOT_RECEIVED (#248)

--- src/main/java/it/pagopa/transactions/projections/handlers/ClosureSendProjectionHandler.java
+++ src/main/java/it/pagopa/transactions/projections/handlers/ClosureSendProjectionHandler.java
@@ -8,6 +8,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import java.util.Optional;
 
 @Component
 @Slf4j
@@ -21,14 +25,23 @@ public class ClosureSendProjectionHandler
         return transactionsViewRepository.findById(event.getTransactionId())
                 .switchIfEmpty(Mono.error(new TransactionNotFoundException(event.getTransactionId())))
                 .flatMap(transactionDocument -> {
-                    TransactionStatusDto newStatus =
+                    Tuple2<TransactionStatusDto, Optional<TransactionUserReceiptData.Outcome>> viewUpdatedData =
                             switch (event) {
-                                case TransactionClosedEvent e -> TransactionStatusDto.CLOSED;
-                                case TransactionClosureFailedEvent e -> TransactionStatusDto.UNAUTHORIZED;
+                                case TransactionClosedEvent e -> Tuples.of(TransactionStatusDto.CLOSED,
+                                        Optional
+                                                .of(e.getData())
+                                                .map(data -> switch (data.getResponseOutcome()) {
+                                                    case OK -> TransactionUserReceiptData.Outcome.NOT_RECEIVED;
+                                                    case KO -> null;
+                                                })
+                                );
+                                case TransactionClosureFailedEvent e ->
+                                        Tuples.of(TransactionStatusDto.UNAUTHORIZED, Optional.empty());
                                 case default -> throw new IllegalArgumentException("Unexpected event: " + event);
                             };
 
-                    transactionDocument.setStatus(newStatus);
+                    transactionDocument.setStatus(viewUpdatedData.getT1());
+                    transactionDocument.setSendPaymentResultOutcome(viewUpdatedData.getT2().orElse(null));
                     return transactionsViewRepository.save(transactionDocument);
                 });
     }
