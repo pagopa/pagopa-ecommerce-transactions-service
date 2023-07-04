170632295cc115d5e86fe63c09329b3b4c7f876f
Giovanni Berti <giovanni.berti@pagopa.it>
2023-07-04 10:22:28 +0200

    feat(tracing): make ecommerce client the default queue client

--- src/main/java/it/pagopa/transactions/configurations/AzureStorageConfig.java
+++ src/main/java/it/pagopa/transactions/configurations/AzureStorageConfig.java
@@ -3,8 +3,8 @@ package it.pagopa.transactions.configurations;
 import com.azure.core.http.HttpHeaders;
 import com.azure.core.http.policy.HttpLogDetailLevel;
 import com.azure.core.util.serializer.JsonSerializer;
-import com.azure.storage.queue.QueueAsyncClient;
 import com.azure.storage.queue.QueueClientBuilder;
+import it.pagopa.ecommerce.commons.client.QueueAsyncClient;
 import it.pagopa.ecommerce.commons.queues.StrictJsonSerializerProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,95 +27,83 @@ public class AzureStorageConfig {
 
     @Bean("transactionActivatedQueueAsyncClient")
     @Qualifier
-    public it.pagopa.ecommerce.commons.client.QueueAsyncClient transactionActivatedQueueAsyncClient(
-                                                                                                    @Value(
-                                                                                                        "${azurestorage.connectionstringtransient}"
-                                                                                                    ) String storageConnectionString,
-                                                                                                    @Value(
-                                                                                                        "${azurestorage.queues.transactionexpiration.name}"
-                                                                                                    ) String queueName,
-                                                                                                    JsonSerializer jsonSerializer
+    public QueueAsyncClient transactionActivatedQueueAsyncClient(
+                                                                 @Value(
+                                                                     "${azurestorage.connectionstringtransient}"
+
+                                                                 ) String storageConnectionString,
+                                                                 @Value(
+                                                                     "${azurestorage.queues.transactionexpiration.name}"
+                                                                 ) String queueName,
+                                                                 JsonSerializer jsonSerializer
     ) {
-        return new it.pagopa.ecommerce.commons.client.QueueAsyncClient(
-                buildQueueAsyncClient(storageConnectionString, queueName),
-                jsonSerializer
-        );
+        return buildQueueAsyncClient(storageConnectionString, queueName, jsonSerializer);
     }
 
     @Bean("transactionRefundQueueAsyncClient")
     @Qualifier
-    public it.pagopa.ecommerce.commons.client.QueueAsyncClient transactionRefundQueueAsyncClient(
-                                                                                                 @Value(
-                                                                                                     "${azurestorage.connectionstringtransient}"
-                                                                                                 ) String storageConnectionString,
-                                                                                                 @Value(
-                                                                                                     "${azurestorage.queues.transactionrefund.name}"
-                                                                                                 ) String queueName,
-                                                                                                 JsonSerializer jsonSerializer
+    public QueueAsyncClient transactionRefundQueueAsyncClient(
+                                                              @Value(
+                                                                  "${azurestorage.connectionstringtransient}"
+                                                              ) String storageConnectionString,
+                                                              @Value(
+                                                                  "${azurestorage.queues.transactionrefund.name}"
+                                                              ) String queueName,
+                                                              JsonSerializer jsonSerializer
     ) {
-        return new it.pagopa.ecommerce.commons.client.QueueAsyncClient(
-                buildQueueAsyncClient(storageConnectionString, queueName),
-                jsonSerializer
-        );
+        return buildQueueAsyncClient(storageConnectionString, queueName, jsonSerializer);
     }
 
     @Bean("transactionClosureRetryQueueAsyncClient")
-    public it.pagopa.ecommerce.commons.client.QueueAsyncClient transactionClosureRetryQueueAsyncClient(
-                                                                                                       @Value(
-                                                                                                           "${azurestorage.connectionstringtransient}"
-                                                                                                       ) String storageConnectionString,
-                                                                                                       @Value(
-                                                                                                           "${azurestorage.queues.transactionclosepaymentretry.name}"
-                                                                                                       ) String queueName,
-                                                                                                       JsonSerializer jsonSerializer
+    public QueueAsyncClient transactionClosureRetryQueueAsyncClient(
+                                                                    @Value(
+                                                                        "${azurestorage.connectionstringtransient}"
+                                                                    ) String storageConnectionString,
+                                                                    @Value(
+                                                                        "${azurestorage.queues.transactionclosepaymentretry.name}"
+                                                                    ) String queueName,
+                                                                    JsonSerializer jsonSerializer
     ) {
-        return new it.pagopa.ecommerce.commons.client.QueueAsyncClient(
-                buildQueueAsyncClient(storageConnectionString, queueName),
-                jsonSerializer
-        );
+        return buildQueueAsyncClient(storageConnectionString, queueName, jsonSerializer);
     }
 
     @Bean("transactionClosureQueueAsyncClient")
-    public it.pagopa.ecommerce.commons.client.QueueAsyncClient transactionClosureQueueAsyncClient(
-                                                                                                  @Value(
-                                                                                                      "${azurestorage.connectionstringtransient}"
-                                                                                                  ) String storageConnectionString,
-                                                                                                  @Value(
-                                                                                                      "${azurestorage.queues.transactionclosepayment.name}"
-                                                                                                  ) String queueName,
-                                                                                                  JsonSerializer jsonSerializer
+    public QueueAsyncClient transactionClosureQueueAsyncClient(
+                                                               @Value(
+                                                                   "${azurestorage.connectionstringtransient}"
+                                                               ) String storageConnectionString,
+                                                               @Value(
+                                                                   "${azurestorage.queues.transactionclosepayment.name}"
+                                                               ) String queueName,
+                                                               JsonSerializer jsonSerializer
     ) {
-        return new it.pagopa.ecommerce.commons.client.QueueAsyncClient(
-                buildQueueAsyncClient(storageConnectionString, queueName),
-                jsonSerializer
-        );
+        return buildQueueAsyncClient(storageConnectionString, queueName, jsonSerializer);
     }
 
     @Bean("transactionNotificationRequestedQueueAsyncClient")
-    public it.pagopa.ecommerce.commons.client.QueueAsyncClient transactionNotificationRequestedQueueAsyncClient(
-                                                                                                                @Value(
-                                                                                                                    "${azurestorage.connectionstringtransient}"
-                                                                                                                ) String storageConnectionString,
-                                                                                                                @Value(
-                                                                                                                    "${azurestorage.queues.transactionnotificationrequested.name}"
-                                                                                                                ) String queueName,
-                                                                                                                JsonSerializer jsonSerializer
+    public QueueAsyncClient transactionNotificationRequestedQueueAsyncClient(
+                                                                             @Value(
+                                                                                 "${azurestorage.connectionstringtransient}"
+                                                                             ) String storageConnectionString,
+                                                                             @Value(
+                                                                                 "${azurestorage.queues.transactionnotificationrequested.name}"
+                                                                             ) String queueName,
+                                                                             JsonSerializer jsonSerializer
     ) {
-        return new it.pagopa.ecommerce.commons.client.QueueAsyncClient(
-                buildQueueAsyncClient(storageConnectionString, queueName),
-                jsonSerializer
-        );
+        return buildQueueAsyncClient(storageConnectionString, queueName, jsonSerializer);
     }
 
     private QueueAsyncClient buildQueueAsyncClient(
                                                    String storageConnectionString,
-                                                   String queueName
+                                                   String queueName,
+                                                   JsonSerializer jsonSerializer
     ) {
         com.azure.storage.queue.QueueAsyncClient queueAsyncClient = new QueueClientBuilder()
                 .connectionString(storageConnectionString)
                 .queueName(queueName)
                 .buildAsyncClient();
         queueAsyncClient.createIfNotExists().block();
-        return queueAsyncClient;
+
+        return new QueueAsyncClient(queueAsyncClient, jsonSerializer);
     }
 }
