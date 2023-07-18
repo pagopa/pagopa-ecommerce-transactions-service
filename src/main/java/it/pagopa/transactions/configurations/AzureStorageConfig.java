d965a99f8e8c06f9985d063790b0bce04c35aaf9
Giovanni Berti <giovanni.berti@pagopa.it>
2023-07-18 14:59:44 +0200

    refactor(serialization): use commons-provided serializer for queue clients

--- src/main/java/it/pagopa/transactions/configurations/AzureStorageConfig.java
+++ src/main/java/it/pagopa/transactions/configurations/AzureStorageConfig.java
@@ -2,14 +2,10 @@ package it.pagopa.transactions.configurations;
 
 import com.azure.core.http.HttpHeaders;
 import com.azure.core.http.policy.HttpLogDetailLevel;
-import com.azure.core.serializer.json.jackson.JacksonJsonSerializerBuilder;
 import com.azure.core.util.serializer.JsonSerializer;
 import com.azure.storage.queue.QueueAsyncClient;
 import com.azure.storage.queue.QueueClientBuilder;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import it.pagopa.ecommerce.commons.queues.StrictJsonSerializerProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -17,7 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 import java.util.Set;
 
 @Configuration
@@ -27,21 +22,7 @@ public class AzureStorageConfig {
 
     @Bean
     public JsonSerializer jsonSerializer() {
-        ObjectMapper queueObjectMapper = new ObjectMapper()
-                .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
-                .configure(DeserializationFeature.FAIL_ON_NULL_CREATOR_PROPERTIES, true)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true)
-                .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
-                .activateDefaultTyping(
-                        BasicPolymorphicTypeValidator.builder()
-                                .allowIfBaseType("it.pagopa.ecommerce")
-                                .allowIfBaseType(List.class)
-                                .build(),
-                        ObjectMapper.DefaultTyping.EVERYTHING,
-                        JsonTypeInfo.As.PROPERTY
-                );
-
-        return new JacksonJsonSerializerBuilder().serializer(queueObjectMapper).build();
+        return new StrictJsonSerializerProvider().createInstance();
     }
 
     @Bean("transactionActivatedQueueAsyncClient")
