d97bdb78141e3e85ca34e386c207e93a9939fcb2
Giovanni Berti <giovanni.berti@pagopa.it>
2023-08-10 16:46:46 +0200

    refactor(config): remove unnecessary interface implementation of `WebfluxConfigurer`

--- src/main/java/it/pagopa/transactions/configurations/NpgWebClientsConfig.java
+++ src/main/java/it/pagopa/transactions/configurations/NpgWebClientsConfig.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
@@ -17,7 +16,7 @@ import reactor.netty.http.client.HttpClient;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
-public class NpgWebClientsConfig implements WebFluxConfigurer {
+public class NpgWebClientsConfig {
 
     @Value("${npg.client.maxInMemory}")
     private int maxMemorySize;
