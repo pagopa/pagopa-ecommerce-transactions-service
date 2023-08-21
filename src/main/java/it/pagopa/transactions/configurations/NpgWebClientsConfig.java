b34cf9edf170fc5b07c0386a5506e28a414d87c1
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2023-08-21 14:49:22 +0200

    remove unused import

--- src/main/java/it/pagopa/transactions/configurations/NpgWebClientsConfig.java
+++ src/main/java/it/pagopa/transactions/configurations/NpgWebClientsConfig.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
