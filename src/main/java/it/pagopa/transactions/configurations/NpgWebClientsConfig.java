43e742ff876a7f096837a4bee8c1cd1fc19dba4b
ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>
2023-08-24 11:06:44 +0200

    feat(npg): [CHK-1782] update commons library (#291)
    * update commons library
    
    * add Npg Client configurations and bean
    
    * fix commons import
    
    * update commons version
    
    * add env var to helm values
    
    * add npg web client config
    
    * add tests
    
    * Update helm/values-prod.yaml
    
    * refactor(config): remove unnecessary interface implementation of `WebfluxConfigurer`
    
    * docs(readme): add missing env vars to readme
    
    * refactor(lint): remove unnecessary `public` modifier in NPG config tests
    
    * remove npg client payload limit
    
    * remove unused import
    
    * update intellij env
    
    ---------
    
    Co-authored-by: Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
    Co-authored-by: Giovanni Berti <giovanni.berti@pagopa.it>

---
+++ src/main/java/it/pagopa/transactions/configurations/NpgWebClientsConfig.java
@@ -0,0 +1,56 @@
+package it.pagopa.transactions.configurations;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import it.pagopa.ecommerce.commons.client.NpgClient;
+import it.pagopa.ecommerce.commons.generated.npg.v1.ApiClient;
+import it.pagopa.ecommerce.commons.generated.npg.v1.api.PaymentServicesApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class NpgWebClientsConfig {
+
+    @Bean(name = "npgWebClient")
+    public PaymentServicesApi npgWebClient(
+                                           @Value("${npg.uri}") String npgWebClientUri,
+                                           @Value(
+                                               "${npg.readTimeout}"
+                                           ) int npgWebClientReadTimeout,
+                                           @Value(
+                                               "${npg.connectionTimeout}"
+                                           ) int npgWebClientConnectionTimeout
+    ) {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, npgWebClientConnectionTimeout)
+                .doOnConnected(
+                        connection -> connection.addHandlerLast(
+                                new ReadTimeoutHandler(
+                                        npgWebClientReadTimeout,
+                                        TimeUnit.MILLISECONDS
+                                )
+                        )
+                );
+
+        WebClient webClient = ApiClient.buildWebClientBuilder().clientConnector(
+                new ReactorClientHttpConnector(httpClient)
+        ).baseUrl(npgWebClientUri).build();
+
+        return new PaymentServicesApi(new ApiClient(webClient));
+    }
+
+    @Bean
+    public NpgClient npgClient(
+                               PaymentServicesApi hostedFieldsApi,
+                               @Value("${npg.client.key}") String npgKey
+    ) {
+        return new NpgClient(hostedFieldsApi, npgKey);
+    }
+
+}
