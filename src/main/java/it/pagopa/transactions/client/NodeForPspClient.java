a2898b9f6d3aa897e87d5f23f4ba4e93a657cd3f
Pietro Tota <pietro.tota@pagopa.it>
2023-11-20 16:25:24 +0100

    fix: add nodo close payment response outcome logging

--- src/main/java/it/pagopa/transactions/client/NodeForPspClient.java
+++ src/main/java/it/pagopa/transactions/client/NodeForPspClient.java
@@ -82,7 +82,8 @@ public class NodeForPspClient {
 
     public Mono<ClosePaymentResponseDto> closePaymentV2(ClosePaymentRequestV2Dto request) {
         log.info(
-                "Requested closePaymentV2 for paymentTokens {} - outcome: {}",
+                "Requested closePaymentV2 for transactionId: [{}]: paymentTokens {} - outcome: {}",
+                request.getTransactionId(),
                 request.getPaymentTokens(),
                 request.getOutcome().getValue()
         );
@@ -111,7 +112,8 @@ public class NodeForPspClient {
                 .doOnSuccess(
                         closePaymentResponse -> log
                                 .info(
-                                        "Received closePayment response for paymentTokens [{}], outcome: [{}]",
+                                        "Received closePaymentV2 response for transactionId: [{}] and : paymentTokens {} - outcome: {}",
+                                        request.getTransactionId(),
                                         request.getPaymentTokens(),
                                         closePaymentResponse.getOutcome()
                                 )
