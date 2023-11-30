441e0885e7942d8b327c8e6d8ffee004c33a4059
Pietro Tota <pietro.tota@pagopa.it>
2023-11-30 11:00:44 +0100

    fix: tests

--- src/test/java/it/pagopa/transactions/services/v2/CircuitBreakerTest.java
+++ src/test/java/it/pagopa/transactions/services/v2/CircuitBreakerTest.java
@@ -1,29 +1,37 @@
 package it.pagopa.transactions.services.v2;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryRegistry;
 import it.pagopa.ecommerce.commons.documents.PaymentNotice;
 import it.pagopa.ecommerce.commons.documents.PaymentTransferInformation;
-import it.pagopa.ecommerce.commons.documents.v1.TransactionActivatedData;
-import it.pagopa.ecommerce.commons.documents.v1.TransactionActivatedEvent;
+import it.pagopa.ecommerce.commons.documents.v2.TransactionActivatedData;
+import it.pagopa.ecommerce.commons.documents.v2.TransactionActivatedEvent;
+import it.pagopa.ecommerce.commons.domain.PaymentToken;
 import it.pagopa.ecommerce.commons.domain.TransactionId;
-import it.pagopa.ecommerce.commons.v1.TransactionTestUtils;
+import it.pagopa.ecommerce.commons.v2.TransactionTestUtils;
 import it.pagopa.generated.transactions.model.CtFaultBean;
-import it.pagopa.generated.transactions.server.model.ClientIdDto;
-import it.pagopa.generated.transactions.server.model.NewTransactionRequestDto;
-import it.pagopa.generated.transactions.server.model.PartyConfigurationFaultDto;
-import it.pagopa.generated.transactions.server.model.PaymentNoticeInfoDto;
+import it.pagopa.generated.transactions.server.model.RequestAuthorizationRequestDto;
+import it.pagopa.generated.transactions.v2.server.model.ClientIdDto;
+import it.pagopa.generated.transactions.v2.server.model.NewTransactionRequestDto;
+import it.pagopa.generated.transactions.v2.server.model.PartyConfigurationFaultDto;
+import it.pagopa.generated.transactions.v2.server.model.PaymentNoticeInfoDto;
 import it.pagopa.transactions.commands.handlers.v2.TransactionActivateHandler;
-import it.pagopa.transactions.exceptions.InvalidNodoResponseException;
-import it.pagopa.transactions.exceptions.NodoErrorException;
-import it.pagopa.transactions.services.v1.TransactionsService;
+import it.pagopa.transactions.exceptions.*;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,8 +40,11 @@ import org.springframework.test.context.TestPropertySource;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static it.pagopa.ecommerce.commons.v1.TransactionTestUtils.EMAIL_STRING;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -52,6 +63,141 @@ class CircuitBreakerTest {
 
     @Autowired
     private CircuitBreakerRegistry circuitBreakerRegistry;
+
+    @Autowired
+    private RetryRegistry retryRegistry;
+
+    private static final JsonNode resilience4jConfiguration;
+
+    private static final Map<String, Exception> exceptionMapper = Map.of(
+            "it.pagopa.transactions.exceptions.UnsatisfiablePspRequestException",
+            new UnsatisfiablePspRequestException(
+                    new PaymentToken(""),
+                    RequestAuthorizationRequestDto.LanguageEnum.IT,
+                    0
+            ),
+            "it.pagopa.transactions.exceptions.PaymentNoticeAllCCPMismatchException",
+            new PaymentNoticeAllCCPMismatchException("rptId", true, true),
+            "it.pagopa.transactions.exceptions.TransactionNotFoundException",
+            new TransactionNotFoundException(""),
+            "it.pagopa.transactions.exceptions.AlreadyProcessedException",
+            new AlreadyProcessedException(
+                    new TransactionId(it.pagopa.ecommerce.commons.v1.TransactionTestUtils.TRANSACTION_ID)
+            ),
+            "it.pagopa.transactions.exceptions.NotImplementedException",
+            new NotImplementedException(""),
+            "it.pagopa.transactions.exceptions.InvalidRequestException",
+            new InvalidRequestException(""),
+            "it.pagopa.transactions.exceptions.TransactionAmountMismatchException",
+            new TransactionAmountMismatchException(10, 11),
+            "it.pagopa.transactions.exceptions.NodoErrorException",
+            new NodoErrorException(new CtFaultBean()),
+            "it.pagopa.transactions.exceptions.InvalidNodoResponseException",
+            new InvalidNodoResponseException("")
+    );
+
+    static {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        try {
+            resilience4jConfiguration = mapper.readTree(new File("./src/main/resources/application.yml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Stream<Arguments> getIgnoredExceptionsForRetry(String retryInstanceName) {
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(
+                        resilience4jConfiguration
+                                .get("resilience4j.retry")
+                                .get("instances")
+                                .get(retryInstanceName)
+                                .get("ignoreExceptions")
+                                .elements(),
+                        Spliterator.ORDERED
+                ),
+                false
+        )
+                .map(json -> {
+                    String exceptionName = json.asText();
+                    return Arguments.of(
+                            Optional
+                                    .ofNullable(exceptionMapper.get(exceptionName))
+                                    .orElseThrow(
+                                            () -> new RuntimeException(
+                                                    "No mapping found for exception: %s".formatted(exceptionName)
+                                            )
+                                    ),
+                            retryInstanceName
+                    );
+                }
+
+                );
+    }
+
+    private static Stream<Arguments> getIgnoredExceptionForNewTransactionRetry() {
+        return getIgnoredExceptionsForRetry("newTransaction");
+    }
+
+    @ParameterizedTest
+    @MethodSource("getIgnoredExceptionForNewTransactionRetry")
+    @Order(0)
+    void shouldNotPerformRetryForExcludedException_newTransactionRetry(
+                                                                       Exception thrownException,
+                                                                       String retryInstanceName
+    ) {
+        Retry retry = retryRegistry.retry(retryInstanceName);
+        long expectedFailedCallsWithoutRetryAttempt = retry.getMetrics().getNumberOfFailedCallsWithoutRetryAttempt()
+                + 1;
+        ClientIdDto clientIdDto = ClientIdDto.CHECKOUT;
+        UUID TEST_CCP = UUID.randomUUID();
+        UUID TRANSACTION_ID = UUID.randomUUID();
+
+        NewTransactionRequestDto transactionRequestDto = new NewTransactionRequestDto()
+                .email(EMAIL_STRING)
+                .addPaymentNoticesItem(new PaymentNoticeInfoDto().rptId(TransactionTestUtils.RPT_ID).amount(10));
+
+        TransactionActivatedData transactionActivatedData = new TransactionActivatedData();
+        transactionActivatedData.setEmail(it.pagopa.ecommerce.commons.v1.TransactionTestUtils.EMAIL);
+        transactionActivatedData
+                .setPaymentNotices(
+                        List.of(
+                                new PaymentNotice(
+                                        it.pagopa.ecommerce.commons.v1.TransactionTestUtils.PAYMENT_TOKEN,
+                                        null,
+                                        "desc",
+                                        0,
+                                        TEST_CCP.toString(),
+                                        List.of(new PaymentTransferInformation("77777777777", false, 0, null)),
+                                        false
+                                )
+                        )
+                );
+
+        TransactionActivatedEvent transactionActivatedEvent = new TransactionActivatedEvent(
+                new TransactionId(TRANSACTION_ID).value(),
+                transactionActivatedData
+        );
+
+        /*
+         * Preconditions
+         */
+        Mockito.when(transactionActivateHandlerV2.handle(any())).thenReturn(Mono.error(thrownException));
+        StepVerifier
+                .create(
+                        transactionsService.newTransaction(
+                                transactionRequestDto,
+                                clientIdDto,
+                                new TransactionId(transactionActivatedEvent.getTransactionId())
+                        )
+                )
+                .expectError(thrownException.getClass())
+                .verify();
+        assertEquals(
+                expectedFailedCallsWithoutRetryAttempt,
+                retry.getMetrics().getNumberOfFailedCallsWithoutRetryAttempt()
+        );
+    }
 
     @Test
     @Order(1)
@@ -147,7 +293,7 @@ class CircuitBreakerTest {
          * Preconditions
          */
         Mockito.when(transactionActivateHandlerV2.handle(any()))
-                .thenReturn(Mono.error(new InvalidNodoResponseException("Invalid response received")));
+                .thenReturn(Mono.error(new RuntimeException("Invalid response received")));
 
         StepVerifier
                 .create(
