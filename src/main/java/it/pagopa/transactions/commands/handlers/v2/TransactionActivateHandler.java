d9b51c04b8bdeedb82fa25ca3b9d472d5d314958
ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>
2023-12-15 08:59:11 +0100

    Update src/main/java/it/pagopa/transactions/commands/handlers/v2/TransactionActivateHandler.java
    Co-authored-by: Fernando-Granato <117268775+Fernando-Granato@users.noreply.github.com>

--- src/main/java/it/pagopa/transactions/commands/handlers/v2/TransactionActivateHandler.java
+++ src/main/java/it/pagopa/transactions/commands/handlers/v2/TransactionActivateHandler.java
@@ -220,7 +220,11 @@ public class TransactionActivateHandler extends TransactionActivateHandlerCommon
                                                 jwtEcommerceValidityTimeInSeconds,
                                                 new Claims(transactionId, command.getData().orderId(), null)
                                         )
-                                        .map(generatedToken -> Tuples.of(generatedToken, paymentRequestInfos))
+                                        
+                                        .fold(
+                                                Mono::error,
+                                                generatedToken -> Mono.just( Tuples.of(generatedToken, paymentRequestInfos)
+                                        )
                                         .fold(
                                                 Mono::error,
                                                 Mono::just
