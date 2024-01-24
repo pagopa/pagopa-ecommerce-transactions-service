a3ce656d6684fbe766caf0d237ad23e7796b48a9
Fernando-Granato <fernando.granato@pagopa.it>
2024-01-24 15:16:08 +0100

    feat: added logic into command for write TRANSACTION_CLOSE_REQUESTED_EVENT

--- src/main/java/it/pagopa/transactions/commands/handlers/TransactionSendClosureRequestHandlerCommon.java
+++ src/main/java/it/pagopa/transactions/commands/handlers/TransactionSendClosureRequestHandlerCommon.java
@@ -3,22 +3,27 @@ package it.pagopa.transactions.commands.handlers;
 import it.pagopa.ecommerce.commons.client.QueueAsyncClient;
 import it.pagopa.ecommerce.commons.documents.BaseTransactionEvent;
 import it.pagopa.ecommerce.commons.queues.TracingUtils;
+import it.pagopa.transactions.commands.TransactionClosureRequestCommand;
+import it.pagopa.transactions.utils.TransactionsUtils;
 import reactor.core.publisher.Mono;
 
 public abstract class TransactionSendClosureRequestHandlerCommon implements
-        CommandHandler<TransactionSendClosureRequestHandlerCommon, Mono<BaseTransactionEvent<?>>> {
+        CommandHandler<TransactionClosureRequestCommand, Mono<BaseTransactionEvent<?>>> {
 
     protected final TracingUtils tracingUtils;
     protected final int transientQueuesTTLSeconds;
+    protected final TransactionsUtils transactionsUtils;
     protected final QueueAsyncClient transactionClosureQueueAsyncClient;
 
     protected TransactionSendClosureRequestHandlerCommon(
             TracingUtils tracingUtils,
             int transientQueuesTTLSeconds,
+            TransactionsUtils transactionsUtils,
             QueueAsyncClient transactionClosureQueueAsyncClient
     ) {
         this.tracingUtils = tracingUtils;
         this.transientQueuesTTLSeconds = transientQueuesTTLSeconds;
+        this.transactionsUtils = transactionsUtils;
         this.transactionClosureQueueAsyncClient = transactionClosureQueueAsyncClient;
     }
 }
