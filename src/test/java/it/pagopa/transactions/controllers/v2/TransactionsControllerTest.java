1f060ef39b0ef36e9b87ed0f628c281cbe377c3d
Fernando-Granato <117268775+Fernando-Granato@users.noreply.github.com>
2024-02-19 09:38:22 +0100

    feat: added correlationId exception 400 (#433)
    * feat: added correlationId exception 400
    
    * feat: added assert for details error

--- src/test/java/it/pagopa/transactions/controllers/v2/TransactionsControllerTest.java
+++ src/test/java/it/pagopa/transactions/controllers/v2/TransactionsControllerTest.java
@@ -416,6 +416,35 @@ class TransactionsControllerTest {
                 );
     }
 
+    @Test
+    void shouldReturnProblemJsonWith400OnMissingCorrelationId() {
+        NewTransactionRequestDto newTransactionRequestDto = new NewTransactionRequestDto()
+                .addPaymentNoticesItem(
+                        new PaymentNoticeInfoDto()
+                                .rptId(TransactionTestUtils.RPT_ID)
+                                .amount(TransactionTestUtils.AMOUNT)
+                )
+                .email("invalidMail")
+                .idCart(TransactionTestUtils.ID_CART);
+        webTestClient.post()
+                .uri("/v2/transactions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("X-Client-Id", "CHECKOUT")
+                .bodyValue(newTransactionRequestDto)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(ProblemJsonDto.class)
+                .value(p -> {
+                    assertEquals(400, p.getStatus());
+                    assertTrue(
+                            p.getDetail().contains(
+                                    "Missing request header 'x-correlation-id' for method parameter of type UUID"
+                            )
+                    );
+                });
+    }
+
     private static CtFaultBean faultBeanWithCode(String faultCode) {
         CtFaultBean fault = new CtFaultBean();
         fault.setFaultCode(faultCode);
