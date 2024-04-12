dc85115c0e410f9ecb67cbe6313043c2bcee9f08
Giovanni Berti <giovanni.berti@pagopa.it>
2024-04-12 10:45:21 +0200

    feat(wallet): handle new fields from `GET auth-data` wallet api

--- src/main/java/it/pagopa/transactions/utils/PaymentSessionData.java
+++ src/main/java/it/pagopa/transactions/utils/PaymentSessionData.java
@@ -2,8 +2,10 @@ package it.pagopa.transactions.utils;
 
 public record PaymentSessionData(
         String cardBin,
+        String lastFourDigits,
         String sessionId,
         String brand,
-        String contractId
+        String contractId,
+        String paypalMaskedEmail
 ) {
 }
