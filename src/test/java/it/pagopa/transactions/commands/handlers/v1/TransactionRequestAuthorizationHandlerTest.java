06e4e95db1a22b6b55dc5c7fcdfb45f7a6a88a65
Giovanni Berti <giovanni.berti@pagopa.it>
2024-06-18 15:57:29 +0200

    refactor: move payment session update on payment methods before call to gateway

--- src/test/java/it/pagopa/transactions/commands/handlers/v1/TransactionRequestAuthorizationHandlerTest.java
+++ src/test/java/it/pagopa/transactions/commands/handlers/v1/TransactionRequestAuthorizationHandlerTest.java
@@ -608,6 +608,13 @@ class TransactionRequestAuthorizationHandlerTest {
                 .state(WorkflowStateDto.CARD_DATA_COLLECTION);
 
         /* preconditions */
+        Mockito.when(
+                paymentMethodsClient.updateSession(
+                        authorizationData.paymentInstrumentId(),
+                        ((CardsAuthRequestDetailsDto) authorizationData.authDetails()).getOrderId(),
+                        transactionId.value()
+                )
+        ).thenReturn(Mono.empty());
         Mockito.when(paymentGatewayClient.requestXPayAuthorization(authorizationData))
                 .thenReturn(Mono.empty());
         Mockito.when(paymentGatewayClient.requestCreditCardAuthorization(authorizationData))
@@ -621,7 +628,7 @@ class TransactionRequestAuthorizationHandlerTest {
                 .expectErrorMatches(error -> error instanceof BadGatewayException)
                 .verify();
 
-        Mockito.verify(paymentMethodsClient, Mockito.times(0)).updateSession(any(), any(), any());
+        Mockito.verify(paymentMethodsClient, Mockito.times(1)).updateSession(any(), any(), any());
     }
 
     @Test
