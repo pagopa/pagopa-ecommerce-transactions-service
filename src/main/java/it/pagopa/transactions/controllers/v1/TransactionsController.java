ccfe3594fcb41091cd0466877d7fa77b07d36c94
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2024-09-17 16:35:57 +0200

    Update src/main/java/it/pagopa/transactions/controllers/v1/TransactionsController.java
    Co-authored-by: Giovanni Berti <giovanni.berti@pagopa.it>

--- src/main/java/it/pagopa/transactions/controllers/v1/TransactionsController.java
+++ src/main/java/it/pagopa/transactions/controllers/v1/TransactionsController.java
@@ -157,11 +157,11 @@ public class TransactionsController implements TransactionsApi {
                             "POST-auth-request-%s".formatted(transactionId),
                             "transactions-service"
                     );
-                    // fix CHK-3222: auth request operations are not idempotent NPG side, so this
+                    // fix CHK-3222: auth request operations are not idempotent on the NPG side, so this
                     // lock prevents multiple auth request to be performed for a single transaction
-                    // (for timeouts scenario and so on). the lock duration have been set to the
-                    // payment token validity time in order to make this api call performable only
-                    // once for transaction (further attempts will found transaction in expired
+                    // (for timeouts scenarios, etc.). The lock duration has been set to the
+                    // payment token validity time in order to make this API call performable only
+                    // once per transaction (further attempts will find the transaction in an expired
                     // status and return an error)
                     boolean lockAcquired = exclusiveLockDocumentWrapper.saveIfAbsent(
                             lockDocument,
