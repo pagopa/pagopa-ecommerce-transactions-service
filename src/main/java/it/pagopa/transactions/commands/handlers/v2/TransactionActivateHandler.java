3c0803bec6f315fbfdbf448b8a77751580c878c2
ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>
2025-06-17 14:37:47 +0200

    Revert "Revert "[CHK-4175] update auth request jwt token generation (#636)"" (#645)
    This reverts commit 1855881a

--- src/main/java/it/pagopa/transactions/commands/handlers/v2/TransactionActivateHandler.java
+++ src/main/java/it/pagopa/transactions/commands/handlers/v2/TransactionActivateHandler.java
@@ -19,7 +19,7 @@ import it.pagopa.ecommerce.commons.queues.TracingUtils;
 import it.pagopa.ecommerce.commons.redis.templatewrappers.v2.PaymentRequestInfoRedisTemplateWrapper;
 import it.pagopa.ecommerce.commons.repositories.v2.PaymentRequestInfo;
 import it.pagopa.ecommerce.commons.utils.OpenTelemetryUtils;
-import it.pagopa.ecommerce.commons.utils.v2.JwtTokenUtils;
+import it.pagopa.ecommerce.commons.client.JwtIssuerClient;
 import it.pagopa.transactions.client.JwtTokenIssuerClient;
 import it.pagopa.transactions.commands.TransactionActivateCommand;
 import it.pagopa.transactions.commands.data.NewTransactionRequestData;
@@ -39,7 +39,6 @@ import reactor.core.scheduler.Schedulers;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
 
-import javax.crypto.SecretKey;
 import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.*;
@@ -68,7 +67,6 @@ public class TransactionActivateHandler extends TransactionActivateHandlerCommon
             @Value("${nodo.parallelRequests}") int nodoParallelRequests,
             TracingUtils tracingUtils,
             OpenTelemetryUtils openTelemetryUtils,
-            @Qualifier("ecommerceSigningKey") SecretKey ecommerceSigningKey,
             @Value("${payment.token.validity}") int jwtEcommerceValidityTimeInSeconds,
             JwtTokenIssuerClient jwtTokenIssuerClient
     ) {
@@ -80,7 +78,6 @@ public class TransactionActivateHandler extends TransactionActivateHandlerCommon
                 nodoParallelRequests,
                 tracingUtils,
                 openTelemetryUtils,
-                ecommerceSigningKey,
                 jwtEcommerceValidityTimeInSeconds
         );
         this.paymentRequestInfoRedisTemplateWrapper = paymentRequestInfoRedisTemplateWrapper;
@@ -247,12 +244,12 @@ public class TransactionActivateHandler extends TransactionActivateHandlerCommon
                                                 UUID userId
     ) {
         Map<String, String> claimsMap = new HashMap<>();
-        claimsMap.put(JwtTokenUtils.TRANSACTION_ID_CLAIM, transactionId.value());
+        claimsMap.put(JwtIssuerClient.TRANSACTION_ID_CLAIM, transactionId.value());
         if (orderId != null) {
-            claimsMap.put(JwtTokenUtils.ORDER_ID_CLAIM, orderId);
+            claimsMap.put(JwtIssuerClient.ORDER_ID_CLAIM, orderId);
         }
         if (userId != null) {
-            claimsMap.put(JwtTokenUtils.USER_ID_CLAIM, userId.toString());
+            claimsMap.put(JwtIssuerClient.USER_ID_CLAIM, userId.toString());
         }
         return claimsMap;
     }
@@ -267,7 +264,7 @@ public class TransactionActivateHandler extends TransactionActivateHandlerCommon
                         claimsMap -> jwtTokenIssuerClient.createJWTToken(
                                 new CreateTokenRequestDto()
                                         .duration(jwtEcommerceValidityTimeInSeconds)
-                                        .audience("ecommerce")
+                                        .audience(JwtIssuerClient.ECOMMERCE_AUDIENCE)
                                         .privateClaims(claimsMap)
                         )
                 ).doOnError(
