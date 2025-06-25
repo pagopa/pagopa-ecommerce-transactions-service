ecf0e6953a369c64ae51c99f92065f30c46fe77b
ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>
2025-06-25 17:00:44 +0200

    PDM-710: add helm valuest for klarna (#652)
    * add helm valuest for klarna
    
    * add klarna configuration
    
    * set code to KLRN
    
    * fix spotless

--- src/test/java/it/pagopa/transactions/client/PaymentGatewayClientTest.java
+++ src/test/java/it/pagopa/transactions/client/PaymentGatewayClientTest.java
@@ -122,7 +122,8 @@ class PaymentGatewayClientTest {
     private final NodeForwarderClient<RedirectUrlRequestDto, RedirectUrlResponseDto> nodeForwarderClient = Mockito
             .mock(NodeForwarderClient.class);
 
-    private static final Set<String> redirectPaymentTypeCodes = Set.of("RBPR", "RBPB", "RBPP", "RPIC", "RBPS", "RICO");
+    private static final Set<String> redirectPaymentTypeCodes = Set
+            .of("RBPR", "RBPB", "RBPP", "RPIC", "RBPS", "RICO", "KLRN");
 
     private static final Map<String, String> redirectPaymentTypeCodeDescription = redirectPaymentTypeCodes.stream()
             .collect(
