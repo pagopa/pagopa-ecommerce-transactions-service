79ced03a2f171244929e12c0b8c4bd0368495ca0
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2025-08-29 17:26:00 +0200

    refactoring for auth reqeusts pipeline

--- src/main/java/it/pagopa/transactions/commands/TransactionUserCancelCommand.java
+++ src/main/java/it/pagopa/transactions/commands/TransactionUserCancelCommand.java
@@ -2,14 +2,23 @@ package it.pagopa.transactions.commands;
 
 import it.pagopa.ecommerce.commons.domain.v2.RptId;
 import it.pagopa.ecommerce.commons.domain.v2.TransactionId;
+import lombok.Getter;
 
+import javax.annotation.Nullable;
 import java.util.List;
+import java.util.UUID;
 
+@Getter
 public final class TransactionUserCancelCommand extends TransactionsCommand<TransactionId> {
+
+    private final UUID xUserId;
+
     public TransactionUserCancelCommand(
             List<RptId> rptIds,
-            TransactionId data
+            TransactionId data,
+            @Nullable UUID xUserId
     ) {
         super(rptIds, TransactionsCommandCode.USER_CANCEL_REQUEST, data);
+        this.xUserId = xUserId;
     }
 }
