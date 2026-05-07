30642cc43e0427d3a1ef64694d79595b8b8866be
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2026-05-07 17:44:18 +0200

    Restore exclusive lock document exception

--- src/main/java/it/pagopa/transactions/exceptions/LockNotAcquiredException.java
+++ src/main/java/it/pagopa/transactions/exceptions/LockNotAcquiredException.java
@@ -4,15 +4,12 @@ import it.pagopa.ecommerce.commons.domain.v2.TransactionId;
 import it.pagopa.ecommerce.commons.repositories.ExclusiveLockDocument;
 import lombok.Getter;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Getter
 public class LockNotAcquiredException extends RuntimeException {
 
     private final TransactionId transactionId;
 
-    private final List<ExclusiveLockDocument> exclusiveLockDocumentList;
+    private final ExclusiveLockDocument exclusiveLockDocument;
 
     public LockNotAcquiredException(
             TransactionId transactionId,
@@ -22,23 +19,7 @@ public class LockNotAcquiredException extends RuntimeException {
                 "Lock not acquired for transaction with id: [%s] and locking key: [%s]"
                         .formatted(transactionId.value(), exclusiveLockDocument.id())
         );
-        this.exclusiveLockDocumentList = List.of(exclusiveLockDocument);
-        this.transactionId = transactionId;
-    }
-
-    public LockNotAcquiredException(
-            TransactionId transactionId,
-            List<ExclusiveLockDocument> exclusiveLockDocumentList
-    ) {
-        super(
-                "Lock not acquired for transaction with id: [%s] and locking key: [%s]"
-                        .formatted(
-                                transactionId.value(),
-                                exclusiveLockDocumentList.stream().map(ExclusiveLockDocument::id)
-                                        .collect(Collectors.joining(", "))
-                        )
-        );
-        this.exclusiveLockDocumentList = exclusiveLockDocumentList;
+        this.exclusiveLockDocument = exclusiveLockDocument;
         this.transactionId = transactionId;
     }
 
