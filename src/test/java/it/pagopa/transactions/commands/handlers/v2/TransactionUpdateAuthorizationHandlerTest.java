827caa9e2ff5e9b8cbb9433f99a244c0ae5dc2d9
ivan.ferrando <ivan.ferrando@pagopa.it>
2026-05-11 15:45:04 +0200

    chore: remove unused import

--- src/test/java/it/pagopa/transactions/commands/handlers/v2/TransactionUpdateAuthorizationHandlerTest.java
+++ src/test/java/it/pagopa/transactions/commands/handlers/v2/TransactionUpdateAuthorizationHandlerTest.java
@@ -47,7 +47,6 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.argThat;
 
 @ExtendWith(MockitoExtension.class)
 class TransactionUpdateAuthorizationHandlerTest {
