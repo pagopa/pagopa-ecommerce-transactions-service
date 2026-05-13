b56d7657715e260576ac61cd7125ea9dec75c896
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2026-05-13 14:42:38 +0200

    Revert "simplify reactive chain"
    This reverts commit 5654b07306451a45eaf286528e248dcab8d62722.

--- src/main/java/it/pagopa/transactions/commands/handlers/v2/TransactionSendClosureRequestHandler.java
+++ src/main/java/it/pagopa/transactions/commands/handlers/v2/TransactionSendClosureRequestHandler.java
@@ -2,6 +2,8 @@ package it.pagopa.transactions.commands.handlers.v2;
 
 import it.pagopa.ecommerce.commons.client.QueueAsyncClient;
 import it.pagopa.ecommerce.commons.documents.BaseTransactionEvent;
+import it.pagopa.ecommerce.commons.documents.v2.TransactionClosureRequestedEvent;
+import it.pagopa.ecommerce.commons.domain.v2.TransactionAuthorizationCompleted;
 import it.pagopa.ecommerce.commons.domain.v2.pojos.BaseTransaction;
 import it.pagopa.ecommerce.commons.generated.server.model.TransactionStatusDto;
 import it.pagopa.ecommerce.commons.queues.QueueEvent;
@@ -17,7 +19,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
-import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
 
 import java.time.Duration;
@@ -41,75 +42,69 @@ public class TransactionSendClosureRequestHandler extends TransactionSendClosure
         this.transactionEventSendClosureRequestRepository = transactionEventSendClosureRequestRepository;
     }
 
+    @Override
     public Mono<BaseTransactionEvent<?>> handle(TransactionClosureRequestCommand command) {
-        return transactionsUtils.reduceV2Events(command.getEvents())
-                .flatMap(transaction -> {
-                    TransactionStatusDto status = transaction.getStatus();
-
-                    // 1. Validazione Stato
-                    if (!isValidStatus(status)) {
-                        log.error("Error: requesting async closure for transaction in state {}", status);
-                        return Mono.error(new AlreadyProcessedException(transaction.getTransactionId()));
-                    }
-
-                    // 2. Definizione dell'evento e del timeout (Logica di business estratta)
-                    return saveEventIfNeeded(transaction, command)
-                            .flatMap(tuple -> sendToQueue(tuple.getT2(), tuple.getT1()));
-                });
-    }
-
-    private boolean isValidStatus(TransactionStatusDto status) {
-        return Set.of(TransactionStatusDto.AUTHORIZATION_COMPLETED, TransactionStatusDto.CLOSURE_REQUESTED)
-                .contains(status);
-    }
-
-    private Mono<Tuple2<Duration, BaseTransactionEvent<?>>> saveEventIfNeeded(
-                                                                              BaseTransaction transaction,
-                                                                              TransactionClosureRequestCommand command
-    ) {
-        if (transaction.getStatus() == TransactionStatusDto.AUTHORIZATION_COMPLETED) {
-            var transactionClosureRequestedEvent = new it.pagopa.ecommerce.commons.documents.v2.TransactionClosureRequestedEvent(
-                    transaction.getTransactionId().value()
-            );
-            return transactionEventSendClosureRequestRepository.save(transactionClosureRequestedEvent)
-                    .map(e -> Tuples.of(Duration.ZERO, e));
-        }
-
-        log.info(
-                "event TRANSACTION_CLOSURE_REQUESTED_EVENT for transactionId {} already present. Processing it.",
-                transaction.getTransactionId()
+        Mono<BaseTransaction> transaction = transactionsUtils.reduceV2Events(
+                command.getEvents()
         );
 
-        var lastEvent = (BaseTransactionEvent<?>) command.getEvents().getLast();
-        return Mono.just(Tuples.of(Duration.ofSeconds(30), lastEvent));
-    }
+        Mono<? extends BaseTransaction> alreadyProcessedError = transaction
+                .doOnNext(t -> log.error("Error: requesting async closure for transaction in state {}", t.getStatus()))
+                .flatMap(t -> Mono.error(new AlreadyProcessedException(t.getTransactionId())));
 
-    private Mono<BaseTransactionEvent<?>> sendToQueue(
-                                                      BaseTransactionEvent<?> event,
-                                                      Duration visibilityTimeout
-    ) {
-        return tracingUtils.traceMono(
-                this.getClass().getSimpleName(),
-                tracingInfo -> transactionClosureQueueAsyncClient.sendMessageWithResponse(
-                        new QueueEvent<>(event, tracingInfo),
-                        visibilityTimeout,
-                        Duration.ofSeconds(transientQueuesTTLSeconds)
-                )
-        )
-                .doOnNext(
-                        response -> log.info(
-                                "Generated and processed event {} for transactionId {}",
-                                event.getClass().getSimpleName(),
-                                event.getTransactionId()
-                        )
-                )
-                .doOnError(
-                        e -> log.error(
-                                "Error processing event for transactionId {} - {}",
-                                event.getTransactionId(),
-                                e.getMessage()
-                        )
-                )
-                .thenReturn(event);
+        return transaction.filter(
+                t -> Set.of(TransactionStatusDto.AUTHORIZATION_COMPLETED, TransactionStatusDto.CLOSURE_REQUESTED)
+                        .contains(t.getStatus())
+        ).switchIfEmpty(alreadyProcessedError)
+                .flatMap(
+                        t -> Mono.just(t)
+                                .filter(tr -> tr.getStatus() == TransactionStatusDto.AUTHORIZATION_COMPLETED)
+                                .cast(TransactionAuthorizationCompleted.class)
+                                .flatMap(trx -> {
+                                    it.pagopa.ecommerce.commons.documents.v2.TransactionClosureRequestedEvent transactionClosureRequestedEvent = new it.pagopa.ecommerce.commons.documents.v2.TransactionClosureRequestedEvent(
+                                            trx.getTransactionId().value()
+                                    );
+
+                                    return transactionEventSendClosureRequestRepository
+                                            .save(transactionClosureRequestedEvent)
+                                            .map(e -> Tuples.of(Duration.ZERO, e));
+                                })
+                                .switchIfEmpty(
+                                        Mono.just(command.getEvents().getLast())
+                                                .cast(TransactionClosureRequestedEvent.class)
+                                                .doOnNext(
+                                                        evt -> log.info(
+                                                                "event TRANSACTION_CLOSURE_REQUESTED_EVENT for transactionId {} already present. Processing it.",
+                                                                evt.getTransactionId()
+                                                        )
+                                                )
+                                                .map(e -> Tuples.of(Duration.ofSeconds(30), e))
+                                )
+                ).flatMap(
+                        visibilityTimeout_event -> tracingUtils.traceMono(
+                                this.getClass().getSimpleName(),
+                                tracingInfo -> transactionClosureQueueAsyncClient
+                                        .sendMessageWithResponse(
+                                                new QueueEvent<>(visibilityTimeout_event.getT2(), tracingInfo),
+                                                visibilityTimeout_event.getT1(),
+                                                Duration.ofSeconds(transientQueuesTTLSeconds)
+                                        )
+                        ).thenReturn(visibilityTimeout_event.getT2())
+                                .doOnError(
+                                        exception -> log.error(
+                                                "Error to generate or processing event TRANSACTION_CLOSURE_REQUESTED_EVENT for transactionId {} - error {}",
+                                                visibilityTimeout_event.getT2().getTransactionId(),
+                                                exception.getMessage()
+                                        )
+                                )
+                                .doOnNext(
+                                        evt -> log.info(
+                                                "Generated and processed event TRANSACTION_CLOSURE_REQUESTED_EVENT for transactionId {}",
+                                                visibilityTimeout_event.getT2().getTransactionId()
+                                        )
+                                )
+
+                );
+
     }
 }
