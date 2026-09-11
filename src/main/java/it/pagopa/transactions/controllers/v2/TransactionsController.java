b4fbee038de7ea8fc435620d27f29ccdd057de5b
SimoneG97 <simone.galante@pagopa.it>
2026-09-11 10:25:26 +0200

    Applied OER logging guidelines

--- src/main/java/it/pagopa/transactions/controllers/v2/TransactionsController.java
+++ src/main/java/it/pagopa/transactions/controllers/v2/TransactionsController.java
@@ -3,6 +3,7 @@ package it.pagopa.transactions.controllers.v2;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import it.pagopa.ecommerce.commons.annotations.Warmup;
 import it.pagopa.ecommerce.commons.domain.v2.TransactionId;
+import it.pagopa.ecommerce.commons.mdcutilities.LogTracingUtils;
 import it.pagopa.ecommerce.commons.utils.OpenTelemetryUtils;
 import it.pagopa.generated.transactions.v2.server.api.V2Api;
 import it.pagopa.generated.transactions.v2.server.model.*;
@@ -28,6 +29,7 @@ import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -84,7 +86,11 @@ public class TransactionsController implements V2Api {
                                                                        ServerWebExchange exchange
     ) {
         return transactionsService.getTransactionInfo(transactionId, xUserId)
-                .doOnNext(t -> log.info("GetTransactionInfo for transactionId completed: [{}]", transactionId))
+                .doOnNext(
+                        t -> LogTracingUtils.loggerTracingUtils()
+                                .success()
+                                .logInfo(log, "GetTransactionInfo completed")
+                )
                 .map(ResponseEntity::ok);
     }
 
@@ -265,7 +271,6 @@ public class TransactionsController implements V2Api {
         }
     )
     ResponseEntity<ProblemJsonDto> jwtTokenGenerationError(JwtIssuerResponseException exception) {
-        log.warn(exception.getMessage());
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         return new ResponseEntity<>(
                 new ProblemJsonDto()
@@ -292,11 +297,17 @@ public class TransactionsController implements V2Api {
                 )
         );
 
-        log.error(
-                "Nodo error processing request with fault code: [" + faultCode + "] mapped to http status code: [" +
-                        response.getStatusCode() + "]",
-                e
-        );
+        LogTracingUtils.loggerTracingUtils()
+                .failure()
+                .details(
+                        Map.of(
+                                "fault_code",
+                                faultCode,
+                                "mapped_status_code",
+                                response.getStatusCode().toString()
+                        )
+                )
+                .logError(log, e, "Nodo error processing request");
         return response;
     }
 
@@ -306,7 +317,6 @@ public class TransactionsController implements V2Api {
         }
     )
     ResponseEntity<ProblemJsonDto> invalidNodoResponse(InvalidNodoResponseException exception) {
-        log.warn(exception.getMessage());
         HttpStatus httpStatus = HttpStatus.BAD_GATEWAY;
         return new ResponseEntity<>(
                 new ProblemJsonDto()
@@ -321,7 +331,6 @@ public class TransactionsController implements V2Api {
     ResponseEntity<ValidationFaultPaymentDataErrorProblemJsonDto> digitalStampNotAllowedHandler(
                                                                                                 DigitalStampNotAllowedForClientException exception
     ) {
-        log.warn(exception.getMessage());
         HttpStatus httpStatus = HttpStatus.NOT_FOUND;
         return new ResponseEntity<>(
                 new ValidationFaultPaymentDataErrorProblemJsonDto()
